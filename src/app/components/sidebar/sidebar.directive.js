362cdc8715976085b0245404feaf5ffcfb558244
Pouyan Heyratpour <pouyan@ronaksoft.com>
2016-05-07 21:29:23 +0000

    Activate the routed place ancestors
    Signed-off-by: Pouyan Heyratpour <pouyan@ronaksoft.com>

--- src/app/components/sidebar/sidebar.directive.js
+++ src/app/components/sidebar/sidebar.directive.js
@@ -3,23 +3,7 @@
 
   angular
     .module('nested')
-    .directive('nestedSidebar', nestedSidebar);
-
-  /** @ngInject */
-  function nestedSidebar() {
-    var directive = {
-      restrict: 'E',
-      templateUrl: 'app/components/sidebar/sidebar.html',
-      scope: {},
-      controller: SidebarController,
-      controllerAs: 'vm',
-      bindToController: true
-    };
-
-    return directive;
-
-    /** @ngInject */
-    function SidebarController(WsService, NestedPlace, $scope) {
+    .controller('SidebarController', function (WsService, NestedPlace, $scope) {
       var vm = this;
 
       vm.places = [];
@@ -27,12 +11,22 @@
 
       WsService.request('account/get_my_places', {}).then(function (data) {
         for (var k in data.places) {
-          $scope.vm.places.push(new NestedPlace(data.places[k]));
+          $scope.sidebarCtrl.places.push(new NestedPlace(data.places[k]));
         }
       }).catch(function (reason) {
 
       });
-    }
-  }
+    })
+    .directive('nestedSidebar', nestedSidebar);
 
+  /** @ngInject */
+  function nestedSidebar() {
+    return {
+      restrict: 'E',
+      templateUrl: 'app/components/sidebar/sidebar.html',
+      controller: 'SidebarController',
+      controllerAs: 'sidebarCtrl',
+      bindToController: true
+    };
+  }
 })();
