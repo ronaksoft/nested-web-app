df985416b0b49057de9ed2f3b7485628166ee334
Pouyan Heyratpour <pouyan@ronaksoft.com>
2016-05-07 20:03:18 +0000

    Events Load More
    Signed-off-by: Pouyan Heyratpour <pouyan@ronaksoft.com>

--- src/app/events/events.controller.js
+++ src/app/events/events.controller.js
@@ -6,7 +6,7 @@
     .controller('EventsController', EventsController);
 
   /** @ngInject */
-  function EventsController($location, AuthService, WsService, NestedEvent, NestedPlace, $scope, $stateParams, $uibModal, $log) {
+  function EventsController($location, AuthService, WsService, NestedEvent, NestedPlace, $scope, $stateParams, $uibModal) {
     var vm = this;
 
     if (!AuthService.isAuthenticated()) {
@@ -16,6 +16,7 @@
       $location.path('/signin').replace();
     }
 
+    vm.moreEvents = true;
     vm.eventGroups = {};
     vm.gformats = {
       daily: 'EEEE d MMM',
@@ -53,9 +54,10 @@
     }
 
     vm.place = new NestedPlace($stateParams.placeId ? { id: $stateParams.placeId } : undefined);
+    vm.placeAncestors = $stateParams.placeId ? $stateParams.placeId.split('.') : undefined;
     vm.filter = $stateParams.filter;
 
-    var parameters = {
+    vm.parameters = {
       skip: 0,
       limit: 25,
       after: 0,
@@ -63,11 +65,11 @@
     };
 
     if (vm.filters.hasOwnProperty(vm.filter)) {
-      parameters['filter'] = vm.filters[vm.filter].filter;
+      vm.parameters['filter'] = vm.filters[vm.filter].filter;
     }
 
     if (vm.place.id) {
-      parameters['place_id'] = vm.place.id;
+      vm.parameters['place_id'] = vm.place.id;
     }
 
     $scope.postView = function (post, url) {
@@ -80,52 +82,63 @@
       });
 
       $scope.thePost = post;
+      $scope.thePost.load();
       $scope.thePost.loadComments();
+      $scope.lastUrl = $location.path();
 
       modal.opened.then(function () {
-        $scope.thePost.load();
+        $location.update_path(url, true);
       });
 
       modal.closed.then(function () {
+        // $location.update_path($scope.lastUrl, true);
+
+        delete $scope.lastUrl;
         delete $scope.thePost;
       });
     };
 
-    WsService.request('timeline/get_events', parameters).then(function (data) {
-      var now = $scope.events.today;
+    vm.load = function () {
+      WsService.request('timeline/get_events', vm.parameters).then(function (data) {
+        var now = $scope.events.today;
+        $scope.events.moreEvents = !(data.events.length < $scope.events.parameters.limit);
+        $scope.events.parameters.skip += data.events.length;
 
-      for (var key in data.events) {
-        var eventData = data.events[key];
-        var event = new NestedEvent(eventData);
+        for (var key in data.events) {
+          var eventData = data.events[key];
+          var event = new NestedEvent(eventData);
 
-        var gkey = null,
-          gtype = null;
-        if (now.getFullYear() === event.date.getFullYear()) {
-          if (now.getMonth() === event.date.getMonth()) {
-            gtype = 'daily';
-            gkey = gtype + '-' + event.date.getDay();
+          var gkey = null,
+            gtype = null;
+          if (now.getFullYear() === event.date.getFullYear()) {
+            if (now.getMonth() === event.date.getMonth()) {
+              gtype = 'daily';
+              gkey = gtype + '-' + event.date.getDay();
+            } else {
+              gtype = 'monthly';
+              gkey = gtype + '-' + event.date.getMonth();
+            }
           } else {
-            gtype = 'monthly';
-            gkey = gtype + '-' + event.date.getMonth();
+            gtype = 'yearly';
+            gkey = gtype + '-' + event.date.getFullYear();
           }
-        } else {
-          gtype = 'yearly';
-          gkey = gtype + '-' + event.date.getFullYear();
+
+          if (!$scope.events.eventGroups.hasOwnProperty(gkey)) {
+            $scope.events.eventGroups[gkey] = {
+              titleFormat: $scope.events.gformats[gtype],
+              type: gtype,
+              date: event.date,
+              set: []
+            };
+          }
+
+          $scope.events.eventGroups[gkey].set.push(event);
         }
+      }).catch(function (data) {
 
-        if (!$scope.events.eventGroups.hasOwnProperty(gkey)) {
-          $scope.events.eventGroups[gkey] = {
-            titleFormat: $scope.events.gformats[gtype],
-            type: gtype,
-            date: event.date,
-            set: []
-          };
-        }
+      });
+    };
 
-        $scope.events.eventGroups[gkey].set.push(event);
-      }
-    }).catch(function (data) {
-
-    });
+    vm.load();
   }
 })();
