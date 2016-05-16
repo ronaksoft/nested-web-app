15563bc17d8d399103f5b2ad0c9d5367efb8daab
Zahra Joulaei <z.joulaei@ronaksoft.com>
2016-05-16 18:08:40 +0430

    add attachment directory and it's styles

---
+++ src/app/components/attachments/attachments.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('nested')
+    .controller('AttachmentsController', AttachmentsController);
+
+  /** @ngInject */
+  function AttachmentsController($scope) {
+  }
+})();
