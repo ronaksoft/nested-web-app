c91a48bba1984da7323c1d13f47a84c9a9ef1793
Pouyan Heyratpour <p.heyratpour@gmail.com>
2016-06-16 01:43:24 +0430

    Forward issue on chrome fix #36
    Signed-off-by: Pouyan Heyratpour <p.heyratpour@gmail.com>

--- src/app/compose/compose.controller.js
+++ src/app/compose/compose.controller.js
@@ -39,7 +39,7 @@
     vm.post = new NestedPost();
     // TODO : attachment preview should be enabled in compose page, Why model controls attachmentPreview??
     vm.post.attachmentPreview = true;
-    if ($stateParams.relation && $stateParams.relation.contains(':')) {
+    if ($stateParams.relation && $stateParams.relation.indexOf(':') > -1) {
       var relation = $stateParams.relation.split(':');
       switch (relation.shift()) {
         case 'fw':
