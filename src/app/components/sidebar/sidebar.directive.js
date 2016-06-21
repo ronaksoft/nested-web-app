250b92fa6b94363e47456e78ad43142906895feb
robzizo <me@robzizo.ir>
2016-06-21 16:02:26 +0430

    Places cache in cache factory of angular(memory) / needs rewrite with pouyan :D :V

--- src/app/components/sidebar/sidebar.directive.js
+++ src/app/components/sidebar/sidebar.directive.js
@@ -3,45 +3,36 @@
 
   angular
     .module('nested')
-    .controller('SidebarController', function ($q, WsService, NestedPlace, $scope, LoaderService) {
+    .controller('SidebarController', function ($q, WsService, NestedPlace, $scope, LoaderService, $cacheFactory) {
       var vm = this;
       vm.places = [];
       vm.tpl = 'app/components/nested/place/row.html';
 
-      /*if (!CacheFactory.get('placesCache')) {
-        CacheFactory.createCache('placesCache', {});
-        LoaderService.inject(WsService.request('account/get_my_places', {}).then(function (data) {
+      if (!$cacheFactory.get('placesCache')) {
+        var cache = $cacheFactory('placesCache');
+        WsService.request('account/get_my_places', {}).then(function (data) {
           var defer = $q.defer();
           for (var k in data.places) {
-            placesCache.put(k, {
-              place: new NestedPlace(data.places[k]),
-            });
-
-            //$scope.sidebarCtrl.places.push(new NestedPlace(data.places[k]));
+            cache.put(k, new NestedPlace(data.places[k]));
           }
-          defer.resolve(placesCache);
+          defer.resolve(cache);
 
           return defer.promise;
         }).then(function () {
           fill();
-        }).catch(function (reason) {
-
-        }));
+        })
       }
       else {
-        var placesCache = CacheFactory.get('placesCache');
         fill();
       }
 
-      var placesCache = CacheFactory.get('placesCache');
-
       function fill() {
+        var cache = $cacheFactory.get('placesCache');
         var i = 0;
-        for (i = 0; i < placesCache.info().size; i++) {
-          vm.places.push(placesCache.get(i).place);
+        for (i = 0; i < cache.info().size; i++) {
+          vm.places.push(cache.get(i));
         }
-      }*/
-
+      }
 
     })
     .directive('nestedSidebar', nestedSidebar);
