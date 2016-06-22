8fdb9e28851a605b312404b4a6c89e289c5cf0bf
Pouyan Heyratpour <pouyan@ronaksoft.com>
2016-06-22 17:35:49 +0430

    Invitations accpet/decline removes from list
    Signed-off-by: Pouyan Heyratpour <pouyan@ronaksoft.com>

--- src/app/events/events.controller.js
+++ src/app/events/events.controller.js
@@ -65,11 +65,16 @@
     });
 
     // Invitations
-    vm.invitations = [];
+    vm.invitations = {
+      length: 0,
+      invites: {}
+    };
     LoaderService.inject(WsService.request('account/get_invitations').then(function (data) {
       for (var k in data.invitations) {
         if (data.invitations[k].place._id) {
-          vm.invitations.push(new NestedInvitation(data.invitations[k]));
+          var invitation = new NestedInvitation(data.invitations[k]);
+          vm.invitations.invites[invitation.id] = invitation;
+          vm.invitations.length++;
         }
       }
 
@@ -77,6 +82,12 @@
         res();
       });
     }));
+    vm.decideInvite = function (invitation, accept) {
+      return invitation.update(accept).then(function (invitation) {
+        vm.invitations.length--;
+        delete vm.invitations.invites[invitation.id];
+      });
+    };
 
     vm.moreEvents = true;
     vm.eventGroups = {};
