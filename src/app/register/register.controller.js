f443dd70bdce89af800a19c005b08b5fb9ef3897
Kayvan NouredinMousa <kayvannm@ronaksoft.com>
2016-06-26 17:11:21 +0430

    validation of create-place , register , edit-profile

--- src/app/register/register.controller.js
+++ src/app/register/register.controller.js
@@ -6,12 +6,24 @@
     .controller('RegisterController', RegisterController);
 
   /** @ngInject */
-  function RegisterController(AuthService, $location) {
+  function RegisterController($scope,
+                              AuthService) {
     var vm = this;
 
     if (AuthService.isInAuthorization()) {
       $location.path('/').replace();
     }
+    $scope.patterns = {
+      password: {
+        letters: /(?=.*[A-Z])(?=.*[a-z])/,
+        symbolOrNumber: /(?=.*[\d~!@#\$%\^&\*\(\)\-_=\+\|\{\}\[\]\?\.])/
+      },
+      email: {
+        all: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+      }
+    };
+    $scope.patterns.password.all = new RegExp($scope.patterns.password.letters.source + $scope.patterns.password.symbolOrNumber.source);
+    console.log($scope.patterns.password.all);
   }
 })();
 
