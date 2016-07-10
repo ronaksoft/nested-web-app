668199b39b1090a283407ef5cab1bf2731aa777c
robzizo <me@robzizo.ir>
2016-07-10 12:01:03 +0430

    search bar refined

--- src/app/messages/messages.controller.js
+++ src/app/messages/messages.controller.js
@@ -47,6 +47,20 @@
       timer;
 
     };
+
+    vm.srch = function () {
+      console.log(arguments);
+      for (var i = 0; i < arguments.length; i++) {
+        var id = arguments[i];
+        var e = document.getElementById(id);
+        console.log(e);
+        if (e.style.display == 'block')
+          e.style.display = 'none';
+        else {
+          e.style.display = 'block';
+        }
+      }
+    };
     $scope.chngSideView = function () {
       $('.maincontainer').toggleClass('tiny');
     };
