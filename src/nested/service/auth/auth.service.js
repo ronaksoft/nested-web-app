eb7b30a0de2769afb5933995839130b84b1d2d7e
Pouyan Heyratpour <pouyan@ronaksoft.com>
2016-07-13 11:02:34 +0430

    Move Auth Constants
    Signed-off-by: Pouyan Heyratpour <pouyan@ronaksoft.com>

--- src/nested/service/auth/auth.service.js
+++ src/nested/service/auth/auth.service.js
@@ -3,21 +3,6 @@
 
   angular
     .module('nested')
-    .constant('NST_AUTH_STATE', {
-      UNAUTHORIZED: 'unauthorized',
-      AUTHORIZING: 'authorizing',
-      AUTHORIZED: 'authorized'
-    })
-    .constant('NST_AUTH_EVENTS', {
-      UNAUTHORIZE: 'unauthorize',
-      AUTHORIZE: 'authorize',
-      AUTHORIZE_FAIL: 'authorize-fail'
-    })
-    .constant('NST_UNREGISTER_REASON', {
-      LOGOUT: 'logout',
-      AUTH_FAIL: 'authorization_fail',
-      DISCONNECT: 'disconnect'
-    })
     .service('NstSvcAuth', NstSvcAuth);
 
   /** @ngInject */
