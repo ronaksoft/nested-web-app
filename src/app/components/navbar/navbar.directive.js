bc81b9a7e97d8d87a652a21996912590d8b53079
robzizo <me@robzizo.ir>
2016-07-13 16:05:05 +0430

    Top navbar (lower than 990px res) aincluded

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -5,6 +5,21 @@
     .module('nested')
     .controller('NavBarController', function($scope, AuthService) {
       $scope.user = AuthService.user;
+
+      $scope.open = function () {
+        console.log(arguments);
+        for (var i = 0; i < arguments.length; i++) {
+          var id = arguments[i];
+          var e = document.getElementById(id);
+          console.log(e);
+          if (e.style.display == 'block')
+            e.style.display = 'none';
+          else {
+            e.style.display = 'block';
+          }
+        }
+      };
+
     })
     .directive('nestedNavbar', nestedNavbar);
 
