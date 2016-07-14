da5a8462a30954dc981aa95dd94cdd6a04d8e782
robzizo <me@robzizo.ir>
2016-07-14 10:49:32 +0430

    top nav Developing

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -5,21 +5,7 @@
     .module('nested')
     .controller('NavBarController', function($scope, AuthService) {
       $scope.user = AuthService.user;
-
-      $scope.open = function () {
-        console.log(arguments);
-        for (var i = 0; i < arguments.length; i++) {
-          var id = arguments[i];
-          var e = document.getElementById(id);
-          console.log(e);
-          if (e.style.display == 'block')
-            e.style.display = 'none';
-          else {
-            e.style.display = 'block';
-          }
-        }
-      };
-
+      $scope.topNavOpen = false;
     })
     .directive('nestedNavbar', nestedNavbar);
 
