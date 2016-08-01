83cf2222c97fe251e1871e7f5ec2339623522547
robzizo <me@robzizo.ir>
2016-08-01 17:14:04 +0430

    detect ellipsis

---
+++ src/app/components/sidebar/ellipsis.directive.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('nested')
+    .directive('nstSidebar', Sidebar);
+
+  /** @ngInject */
+  function Sidebar() {
+    return {
+      restrict: 'E',
+      templateUrl: 'app/components/sidebar/sidebar.html',
+      controller: 'SidebarController',
+      controllerAs: 'ctlSidebar',
+      bindToController: {
+        collapsed: '='
+      }
+    };
+  }
+})();
