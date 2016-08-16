0fd3d4f32f1e57b6d226e2a1d4b61aec4a226aac
kayvan noureddin moosa <git.ronaksoft.com:kayvannm/nested.git>
2016-08-16 12:14:21 +0430

    new regex tested and added

--- src/app/index.const.js
+++ src/app/index.const.js
@@ -72,8 +72,9 @@
       ]
     })
     .constant('NST_PATTERN', {
-      GRAND_PLACE_ID: /^[a-zA-Z]\w{3,30}[a-zA-Z0-9]$/,
-      SUB_PLACE_ID: /^[a-zA-Z]\w{1,30}[a-zA-Z0-9]$/,
+      USERNAME : /^[a-zA-Z](?!.*--)[a-zA-Z0-9-]{3,30}[^-]$/,
+      GRAND_PLACE_ID: /^[a-zA-Z](?!.*--)[a-zA-Z0-9-]{4,30}[^-]$/,
+      SUB_PLACE_ID: /^[a-zA-Z](?!.*--)[a-zA-Z0-9-]{0,30}[^-]/,
       EMAIL: /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
     })
     .constant('NST_DELIMITERS', {
