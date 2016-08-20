c435342e50379ebbd2ed1eada5ee125520b83cf6
kayvan noureddin moosa <git.ronaksoft.com:kayvannm/nested.git>
2016-08-20 13:30:46 +0430

    text/corrections = final version, and validations are done.

--- src/app/pages/places/add/place-add.controller.js
+++ src/app/pages/places/add/place-add.controller.js
@@ -367,9 +367,9 @@
 
     vm.placeIdMinLength = true;
     vm.existCheck = function (val) {
-      if (vm.model.id && vm.model.id.length < 6){
+      if (vm.model.id.length < 6){
         vm.placeIdMinLength = true;
-        return false;
+
       }else{
         vm.placeIdMinLength = false;
       }
