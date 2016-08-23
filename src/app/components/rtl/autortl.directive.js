e5c75c905a0c6daef4aac24de82b579cf0f46ae1
robzizo <me@robzizo.ir>
2016-08-23 17:36:43 +0430

    auto rtl directive

--- src/app/components/rtl/autortl.directive.js
+++ src/app/components/rtl/autortl.directive.js
@@ -9,16 +9,23 @@
     return {
       restrict: 'A',
       link: function (scope ,element, attrs) {
-        var str = attrs.autoDir;
-        if (!str || !_.isString(str)) {
-          return element.attr("dir","ltr");
-        }
+        scope.$watch(function () {
+          return attrs.autoDir;
+        }, function () {
+          var str = attrs.autoDir;
 
-        str = str.trim();
-        var charCode = str.charCodeAt(0);
-        if (charCode > 1300 && 1700 > charCode) {
-          return element.attr("dir","rtl");
-        }
+
+          if (!str || !_.isString(str)) {
+            return element.attr("dir","ltr");
+          }
+
+          str = str.trim();
+          var charCode = str.charCodeAt(0);
+          if (charCode > 1300 && 1700 > charCode) {
+            return element.attr("dir","rtl");
+          }
+        });
+
       }
     };
   }
