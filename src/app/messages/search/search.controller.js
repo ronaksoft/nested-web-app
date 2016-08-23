130a54d72d78763ebd14933ca6cbe188b6e8dea3
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2016-08-23 18:59:30 +0430

    add search input and buttons on top
    fix #103

--- src/app/messages/search/search.controller.js
+++ src/app/messages/search/search.controller.js
@@ -39,6 +39,7 @@
     vm.search = search;
     vm.loadMore = loadMore;
     vm.searchOnEnterKeyPressed = searchOnEnterKeyPressed;
+    vm.clearSearchQuery = clearSearchQuery;
 
     (function () {
 
@@ -67,7 +68,6 @@
 
     function search(queryString) {
       vm.messages.length = 0;
-      skip = 0;
       var query = new NstSearchQuery(queryString);
       $state.go('search', { query : NstSearchQuery.encode(queryString) } , { notify : false }).then(function (newState) {
         skip = 0;
@@ -109,75 +109,14 @@
       searchMessages(vm.queryString);
     }
 
-
+    function clearSearchQuery() {
+      vm.queryString = '';
+      search('');
+    }
 
     vm.bodyScrollConf = {
       axis: 'y',
       callbacks: {
-        // whileScrolling: function () {
-        //   var t = -this.mcs.top;
-        //   //$timeout(function () { $rootScope.navView = t > 55; });
-        //   //console.log(tl);
-        //   // tl.kill({
-        //   //   y: true
-        //   // }, cp);
-        //   // TweenLite.to(cp, 0.5, {
-        //   //   y: 140,
-        //   //   ease: Power2.easeOut,
-        //   //   force3D: true
-        //   // });
-        //   if (t > 55 && !$rootScope.navView) {
-        //     //$('#content-plus').clone().prop('id', 'cpmirror').appendTo("#mCSB_3_container");
-        //     //var z = $('#content-plus').offset().left + 127;
-        //     //console.log(z);
-        //     //$('#cpmirror').css({'opacity':0});
-        //     //$('#content-plus').css({position: 'fixed',marginLeft: 356});
-        //     //tl.kill({minHeight:true,maxHeight:true}, nav);
-        //     // TweenLite.to(nav, 0.1, {
-        //     //   minHeight: 96,
-        //     //   maxHeight: 96,
-        //     //   height: 96,
-        //     //   ease: Power1.easeOut
-        //     // });
-        //     $timeout(function () {
-        //       $rootScope.navView = t > 55;
-        //     });
-        //   } else if (t < 55 && $rootScope.navView) {
-        //     // TweenLite.to(nav, 0.1, {
-        //     //   minHeight: 183,
-        //     //   maxHeight: 183,
-        //     //   height: 183,
-        //     //   ease: Power1.easeOut
-        //     // });
-        //     $timeout(function () {
-        //       $rootScope.navView = t > 55;
-        //     });
-        //   }
-        //
-        //   //tl.lagSmoothing(200, 20);
-        //   tl.play();
-        //   // $("#content-plus").stop().animate(
-        //   //   {marginTop:t}, {duration:1});
-        //   // TweenMax.to("#cp1", .001, {
-        //   //   y: t, ease:SlowMo.ease.config(0.7, 0.7, true)
-        //   // });
-        //   //TweenMax.lagSmoothing(500, 33);
-        //
-        //
-        //   //   var func = function () {
-        //   //     console.log(t);
-        //   //     $("#content-plus").animate(
-        //   //       {marginTop:t}, {duration:1, easing:"easeOutStrong"});
-        //   //   };
-        //   //   var debounced = _.debounce(func, 250, { 'maxWait': 1000 });
-        //   //   if ( t > 0) {
-        //   //     debounced();
-        //   //   } else if(t == 0){
-        //   //   $("#content-plus").stop().css({
-        //   //     marginTop: 0
-        //   //   });
-        //   // }
-        // },
         onTotalScroll: function () {
           vm.loadMore();
         },
