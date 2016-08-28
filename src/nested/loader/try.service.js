9b61dade587d7d7b372495af559d57a58865ece0
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2016-08-28 14:46:55 +0430

    add documentation for try service
    fix #118

--- src/nested/loader/try.service.js
+++ src/nested/loader/try.service.js
@@ -13,6 +13,15 @@
     Try.prototype = new NstObservableObject();
     Try.prototype.constructor = Try;
 
+    /**
+     * anonymous function - Retries a function
+     *
+     * @param  {Function}   method    A function to be executed
+     * @param  {Function}   cutoff    Stops retrying if the callback returns true
+     * @param  {Number}     tries     Number of maximum chances for retry
+     * @param  {Number}     delay     Milliseconds between every retry
+     * @return {Promise}              The result of retrying the function
+     */
     Try.prototype.do = function (method, cutoff, tries, delay) {
       var service = this;
 
@@ -21,9 +30,11 @@
       cutoff = cutoff || function () { return false; };
 
       return method().then(function (result) {
+        // do not try anymore
         return $q.resolve(result);
       }).catch(function (error) {
         if (cutoff(error) || tries === 0) {
+          //
           return $q.reject(error);
         } else {
           return $timeout(function () {
