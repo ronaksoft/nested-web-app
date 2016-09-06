dc2de3d0b61c44ba81be7a3e6c4b362f9a2a7d57
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2016-09-06 20:34:58 +0430

    Restore unseen activities to the default color
    fix #142

--- src/app/events/set-seen-on-visibile.directive.js
+++ src/app/events/set-seen-on-visibile.directive.js
@@ -7,16 +7,17 @@
       return {
         restrict: 'A',
         link: function (scope, element, attrs) {
+          var key = attrs.eventKey || 'body-scroll-change';
           var content = $(element);
-          content.on('scroll', function () {
-            var recents = content.find('.hot');
-            $.each(recents, function (index, e) {
-              var element = $(e);
-              if (element.hasClass('hot') && element.visible()) {
-                element.removeClass('hot');
-                element.addClass('seen');
+          scope.$on(key, function (event) {
+
+            $.each(content.find('.hot'), function (index, i) {
+              var item = $(i);
+              if (item.hasClass('hot') && item.visible()) {
+                item.addClass('seen');
               }
             });
+
           });
         }
       };
