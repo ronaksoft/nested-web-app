967f5eb8881fb7fa7f0cb1f49e74bcfde17bdd80
shayesteh <naeimabadi.shayesteh@gmail.com>
2016-09-14 14:56:42 +0430

    atwho mention ready for connect to api

--- src/app/components/mention/mention.directive.js
+++ src/app/components/mention/mention.directive.js
@@ -8,7 +8,7 @@
         link: function (scope, element, attrs) {
 
           var data = [
-            {'name':"Shayesteh Naeimabadi", 'id':"robzizo", 'imageUrl': "http://xerxes.nested.ronaksoftware.com/view/57d78985b074b1536c59cc57/THU57CFECBEA203D57CFECBEB074B103987227BB/"},
+            {'name':"Shayesteh Naeimabadi", 'id':"robzizo", 'imageUrl': "http://xerxes.nested.ronaksoftware.com/view/57d8db65b074b1536900afe9/THU57CFECBEA203D57CFECBEB074B103987227BB/"},
             {'name':"ali Mahmoodi", 'id':"naamesteh", 'imageUrl': "http://xerxes.nested.ronaksoftware.com/view/57d8db65b074b1536900afe9/THU57D66D409D35057D66D40B074B110AB431297/"},
             {'name':"Pouya Amirahmadi", 'id':"pouya",'imageUrl': "http://xerxes.nested.ronaksoftware.com/view/57d8db65b074b1536900afe9/THU57C2C3A00142257C2C3A0B074B12F5A43DCC5/"}
           ];
@@ -23,8 +23,8 @@
             element.attr("mention", true);
           });
 
-          scope.tplUrlAt = "<div class='box-menu'><ul><li><img src='${imageUrl}' class='account-initials-32 mCS_img_loaded '><span class=' _dts list-unstyled text-centerteammate-name  nst-mood-solid text-name mention-items'>  ${name}</span><span class='_dtr nst-mood-storm nst-font-small'>${id}</span></li></ul></div>";
-          scope.tplUrlHas = "<div class='box-menu'><ul><li><img src='${imageUrl}' class='account-initials-32 mCS_img_loaded '><span class='_dts list-unstyled text-centerteammate-name nst-mood-solid text-name mention-items'>  ${name}</span><span class='_dtr nst-mood-storm nst-font-small'>${id}</span></li></ul></div>";
+          scope.tplUrlAt = "<li class='_difv'><img src='${imageUrl}' class='account-initials-32 mCS_img_loaded _df'><div class='_difv'><span class='_df list-unstyled text-centerteammate-name  nst-mood-solid text-name'>  ${name}</span><span class='_df nst-mood-storm nst-font-small'>${id}</span></div></li>";
+          scope.tplUrlHas = "<li class='_difv'><img src='${imageUrl}' class='account-initials-32 mCS_img_loaded _df'><div class='_difv'><span class='_df list-unstyled text-centerteammate-name  nst-mood-solid text-name'>  ${name}</span><span class='_df nst-mood-storm nst-font-small'>${id}</span></div></li>";
           element
             .atwho({at:"@", 'data':data, searchKey:"name", limit:5, displayTpl:scope.tplUrlAt})
 
