d53c6ae7bf57c8d917a311f9e591aef7ffe4e4aa
Sina <e.hosseini.ir@gmail.com>
2016-09-18 11:30:50 +0430

    Fix mention issues

--- src/app/components/mention/mention.directive.js
+++ src/app/components/mention/mention.directive.js
@@ -1,19 +1,19 @@
-(function (){
-    'use strict';
- angular
-     .module('nested')
-     .directive('nstMention', function ($timeout , NstSvcUserFactory, NstSvcPlaceFactory, NstVmUser, NstVmPlace) {
-    return {
+(function () {
+  'use strict';
+  angular
+    .module('nested')
+    .directive('nstMention', function ($rootScope, $timeout, NstSvcUserFactory, NstSvcPlaceFactory, NstVmUser, NstVmPlace) {
+      return {
         restrict: 'A',
         link: function (scope, element, attrs) {
 
-          element.on("hidden.atwho", function(event, flag, query) {
-            $timeout(function(){
+          element.on("hidden.atwho", function (event, flag, query) {
+            $timeout(function () {
               element.attr("mention", false);
-            },200);
+            }, 200);
           });
 
-          element.on("shown.atwho", function(event, flag, query) {
+          element.on("shown.atwho", function (event, flag, query) {
             element.attr("mention", true);
           });
 
@@ -21,78 +21,84 @@
 
           element
             .atwho({
-              at:"@",
-              searchKey:"name",
+              at: "@",
+              searchKey: "name",
               maxLen: 10,
               startWithSpace: true,
-              limit:5, 
-              displayTpl:scope.tplUrl,
-              callbacks : {    
-                  beforeInsert:function (value, $li){
+              limit: 5,
+              displayTpl: scope.tplUrl,
+              callbacks: {
+                beforeInsert: function (value, $li) {
                   var elm = angular.element($li);
                   return '@' + elm.attr('data-id');
                 },
-                  remoteFilter : function (query, callback) {
-                      var searchSettings = {
-                           query : query,
-                           limit : 5,
-                       };
-                      NstSvcUserFactory.search(searchSettings).then(function (users) {
-                          var items = [];
-                          _.map(users, function (item) {
-                                            var obj =  new NstVmUser(item);
-                                          items.push({
-                                                id : obj.id,
-                                                name : obj.name,
-                                                avatar : obj.avatar
-                                                    })
-                                            });
-                          callback(items);
-                      }).catch(function (error) {
-                      }); 
-                   }
+                remoteFilter: function (query, callback) {
+                  var searchSettings = {
+                    query: query,
+                    limit: 5,
+                  };
+                  NstSvcUserFactory.search(searchSettings).then(function (users) {
+                    var items = [];
+                    _.map(users, function (item) {
+                      var obj = new NstVmUser(item);
+                      items.push({
+                        id: obj.id,
+                        name: obj.name,
+                        avatar: obj.avatar
+                      })
+                    });
+                    callback(items);
+                  }).catch(function (error) {
+                  });
                 }
-              })
-          
+              }
+            })
 
             .atwho({
-                    at:"#", 
-                    searchKey:"name",
-                    maxLen: 10,
-                    startWithSpace: true,
-                    limit:5,
-                    displayTpl:scope.tplUrl,
-                    callbacks : {        
-                          beforeInsert:function (value, $li){
-                          var elm = angular.element($li);
-                          return '#' + elm.attr('data-id');
-                      },
-                    remoteFilter : function (query, callback) {
-                       var searchSettings = {
-                           query : query.query,
-                           limit : 5,
-                      };
-                    NstSvcPlaceFactory.search(query).then(function (places) {
-                           var items = [];
-                         _.map(places, function (item) {
-                                            var obj =  new NstVmPlace(item);
-                            
-                                       items.push({
-                                                id : obj.id,
-                                                name : obj.name,
-                                                avatar : obj.avatar
-                                                 })
-                                      });
-                        
-                             callback(items);
-                      }).catch(function (error) {
-                      });
-                     
-                   }
-              }})
+              at: "#",
+              searchKey: "name",
+              maxLen: 10,
+              startWithSpace: true,
+              limit: 5,
+              displayTpl: scope.tplUrl,
+              callbacks: {
+                beforeInsert: function (value, $li) {
+                  var elm = angular.element($li);
+                  return '#' + elm.attr('data-id');
+                },
+                remoteFilter: function (query, callback) {
+                  var searchSettings = {
+                    query: query.query,
+                    limit: 5,
+                  };
+                  NstSvcPlaceFactory.search(query).then(function (places) {
+                    var items = [];
+                    _.map(places, function (item) {
+                      var obj = new NstVmPlace(item);
 
-            }
-          }
-      })
-       
+                      items.push({
+                        id: obj.id,
+                        name: obj.name,
+                        avatar: obj.avatar
+                      })
+                    });
+
+                    callback(items);
+                  }).catch(function (error) {
+                  });
+
+                }
+              }
+            });
+
+
+          //remove useless atwho-container tag after change a state
+            $rootScope.$on('$stateChangeSuccess', function () {
+              angular.element(".atwho-container").remove();
+            });
+
+        }
+      }
+    })
+
 })();
