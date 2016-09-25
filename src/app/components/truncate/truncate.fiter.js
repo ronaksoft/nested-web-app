6d3d53a01e8daa88110e17976952f5de30ba5cae
shayesteh <naeimabadi.shayesteh@gmail.com>
2016-09-25 15:08:31 +0330

    clean code of truncate filter

--- src/app/components/truncate/truncate.fiter.js
+++ src/app/components/truncate/truncate.fiter.js
@@ -10,8 +10,6 @@
     })
     .filter('truncate', function () {
       return function (content) {
-          console.log("yes");
-              
         var maxCharacters = 250;
 
         if (content == null) return "";
@@ -21,7 +19,7 @@
         if (content.length <= maxCharacters) return content;
 
         return content.substr(0, 247) + '...';
-          
+
       };
     });
 })();
