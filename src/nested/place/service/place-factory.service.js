ee5ddc34dae9046c6f6161ba220e0cacc90c2ce7
Sina <e.hosseini.ir@gmail.com>
2016-10-04 16:30:27 +0330

    Make Tree

--- src/nested/place/service/place-factory.service.js
+++ src/nested/place/service/place-factory.service.js
@@ -8,7 +8,7 @@
   function NstSvcPlaceFactory($q, $log,
                               NST_SRV_ERROR, NST_SRV_EVENT, NST_PLACE_ACCESS, NST_PLACE_MEMBER_TYPE, NST_EVENT_ACTION, NST_PLACE_FACTORY_EVENT, NST_PLACE_POLICY,
                               NstSvcServer, NstSvcPlaceStorage, NstSvcTinyPlaceStorage, NstSvcMyPlaceIdStorage, NstSvcUserFactory, NstSvcPlaceRoleStorage, NstSvcPlaceAccessStorage,
-                              NstObservableObject, NstFactoryQuery, NstFactoryError, NstUtility, NstTinyPlace, NstPlace, NstFactoryEventData,
+                              NstObservableObject, NstFactoryQuery, NstFactoryError, NstUtility, NstTinyPlace, NstPlace, NstFactoryEventData,NstSvcPlaceMap,
                               NstPlaceCreatorOfParentError, NstPlaceOneCreatorLeftError) {
     function PlaceFactory() {
       var factory = this;
@@ -1357,6 +1357,11 @@
       return this.filterPlacesByAccessCode(places, NST_PLACE_ACCESS.READ);
     }
 
+    PlaceFactory.prototype.getChildTree = function (grandPlace, children) {
+        var mapper = new NstSvcPlaceMap();
+        mapper.toTree(children);
+    };
+
     PlaceFactory.prototype.filterPlacesByAccessCode = function (places, code) {
       var defer = $q.defer();
       var factory = this;
@@ -1402,7 +1407,7 @@
       });
 
       return deferred.promise;
-    }
+    };
 
     PlaceFactory.prototype.getGrandPlaceChildren = function (grandPlaceId) {
       var deferred = $q.defer();
@@ -1412,10 +1417,11 @@
       NstSvcServer.request('place/get_sub_places', {
         place_id : grandPlaceId
       }).then(function (data) {
-        console.log('subplaces', data);
+        console.log('subplaces', NstSvcPlaceMap.toTree(data.places));
 
         if (_.isArray(data.places) && !_.isEmpty(data.places)) {
           places = data.places;
+
         }
 
         return NstSvcServer.request('bookmark/get_places', {
--- src/nested/place/service/place-map.service.js
+++ src/nested/place/service/place-map.service.js
@@ -1,40 +1,75 @@
-// (function() {
-//   'use strict';
-//   angular
-//     .module('ronak.nested.web.place')
-//     .service('NstSvcPlaceMap', NstSvcPlaceMap);
-//
-//   /** @ngInject */
-//   function NstSvcPlaceMap() {
-//
-//     var service = {
-//       toSidebarSubPlace : toSidebarSubPlace
-//     };
-//
-//     return service;
-//
-//     function toSidebarSubPlace(grandPlace, places) {
-//       if (!place || !place.id) {
-//         throw 'The grand-place is not provided';
-//       }
-//
-//       if (!(_.isArray(places) && places.length > 0)) {
-//         return [];
-//       }
-//       var children = _.filter(places, function (place) {
-//         return _.startsWith(place.id, grandPlace.id);
-//       });
-//
-//       createMap(grandPlace, children);
-//     }
-//
-//     function createTree(place, places) {
-//       place.children
-//       var placeGroups = _.groupBy(places, function (place) {
-//         return _.
-//       });
-//     }
-//
-//   }
-//
-// })();
+(function() {
+  'use strict';
+  angular
+    .module('ronak.nested.web.place')
+    .service('NstSvcPlaceMap', NstSvcPlaceMap);
+
+  /** @ngInject */
+  function NstSvcPlaceMap() {
+
+    var service = {
+      toTree: toTree
+    };
+
+    return service;
+
+    function toTree(places) {
+
+      var parentsStack = [];
+
+      var setSuperUserLevel = function (subPlace) {
+        var id = subPlace._id;
+        subPlace.children =  [];
+
+        for (var i = parentsStack.length -1 ; i > -1; i--) {
+          if (id.indexOf(parentsStack[i]) === 0){
+            subPlace.level =  i + 1;
+            subPlace.parent = parentsStack[i]
+            return subPlace;
+          }else{
+            parentsStack = _.dropRight(parentsStack)
+          }
+        }
+        subPlace.level =  0;
+        return subPlace;
+      };
+
+      var setLevel = function (subPlace){
+        subPlace = setSuperUserLevel(subPlace);
+        parentsStack.push(subPlace._id);
+      };
+
+
+      var breedPlaces = function(places){
+        var tree = [];
+        for (var i = places.length - 1; i > -1 ; i--) {
+          if (places[i].level === 0){
+            tree.unshift(places[i]);
+          }else{
+
+            places.filter(function(place){
+              return place._id === places[i].parent;
+            })[0].children.unshift(places[i]);
+
+          }
+          _.drop(places);
+        }
+        return tree;
+      };
+
+
+      var sorteedPlaces = _.sortBy(places,['_id']);
+
+      _.forEach(sorteedPlaces, setLevel);
+
+      console.log(3333, breedPlaces(sorteedPlaces));
+      return breedPlaces(sorteedPlaces);
+
+    }
+
+
+
+
+  }
+
+})();
