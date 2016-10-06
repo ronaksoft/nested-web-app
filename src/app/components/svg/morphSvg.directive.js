0c50227dace7b1f4a7038d99c0d4b562ab2449ee
robzizo <me@robzizo.ir>
2016-10-06 14:37:48 +0330

    font isze of post view edited + svg icon of add place

--- src/app/components/svg/morphSvg.directive.js
+++ src/app/components/svg/morphSvg.directive.js
@@ -19,7 +19,6 @@
     };
 
     function link($scope, $element, $attrs) {
-      console.log('hello',$element,$attrs.firstId);
 
       var customSettings = {
         //wheelSpeed : readNumberValue($attrs.wheelSpeed),
@@ -41,27 +40,19 @@
 
       var container = $element[0];
       var jContainer = $(container);
-      var jContainers = $('#icons');
+      var icon = $('#icons');
 
-      var tl = new TimelineLite(),
-        circle = document.getElementById("circle"),
-        first = document.getElementById($attrs.firstId),
-        second = document.getElementById($attrs.secondId);
 
-      // tl.to(circle, 1, {morphSVG:"#hippo"}, "+=1")
-      //   .to(circle, 1, {morphSVG:"#star"}, "+=1")
-      //   .to(circle, 1, {morphSVG:"#elephant"}, "+=1")
-      //   .to(circle, 1, {morphSVG:circle}, "+=1");
 
       jContainer.mouseenter(function () {
-        console.log(jContainers,first);
-        TweenLite.to(first, 1, {morphSVG:{shape:second, shapeIndex:1}});
+        console.log($('#sq40'));
       });
       jContainer.mouseleave(function () {
-        TweenLite.to(second, 1, {morphSVG:{shape:first, shapeIndex:1}});
-      })
+
+      });
 
     }
   }
 
 })();
+
