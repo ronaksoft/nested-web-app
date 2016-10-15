adfde7e5897a524b563c132f987698b30ec8d769
kayvan <kayvan@localhost.localdomain>
2016-10-15 09:49:48 +0330

    e2e testing first touch.

--- e2e/features/user/login.feature
+++ e2e/features/user/login.feature
@@ -25,3 +25,56 @@
 #    Given I Press "Sign in"
 #    When I wait 10s
 #    Then should the title of the place be "All Places"
+#
+#  Scenario: edit profiel
+#    Given I Click Link by Partial Text "@test1"
+#    When I Wait till line loader hide
+#    When I wait 2s
+#    Given I Click Link by Partial Text "Edit My profile"
+#    When I Wait till line loader hide
+#    When I wait 2s
+#    Given I Click Link by Partial Text "Change Password"
+#    When I Wait till line loader hide
+#    When I wait 2s
+#    Then should the title of the place be "Change Password"
+#
+#  Scenario: change password
+#    Given I fill "Old Password" with "111111"
+#    When I wait 2s
+#    Given I fill "New Password" with "111111"
+#    When I wait 2s
+#    Given I fill "New Password Confirm" with "111111"
+#    When I wait 2s
+#    Given I Press "Change"
+#    Then Wait to see success-msg
+#
+#   Scenario: edit profile
+#     When I wait 5s
+#     Given I clear input by name "firstName"
+#     When I wait 2s
+#     Given I fill input by name "firstName" with "test2"
+#     When I wait 2s
+#     Given I clear input by name "lastName"
+#     When I wait 2s
+#     Given I fill input by name "lastName" with "test2"
+#     When I wait 2s
+#     Given I Click Dropdown by Placeholder "Month"
+#     When I wait 2s
+#     Given I Click Option by Label "October"
+#     Given I Click Dropdown by Placeholder "Day"
+#     When I wait 2s
+#     Given I Click Option by Label "20"
+#     Given I Click Dropdown by Placeholder "Year"
+#     When I wait 2s
+#     Given I Click Option by Label "1980"
+#     When I wait 5s
+#     Given I Press "Save & Exit"
+#     When I wait 5s
+#     Then should the title of the place be "Profile"
+#
+#   Scenario: sign out
+#     When I Wait till line loader hide
+#     Given I Click Link by Partial Text "Sign out"
+#     When I Wait till line loader hide
+#     When I wait 5s
+#     Then should the current url be "/signin"
--- e2e/steps/step-definitions.js
+++ e2e/steps/step-definitions.js
@@ -8,10 +8,26 @@ module.exports = function () {
   this.Given(/^I go to the page "([^"]*)"$/, function (url) {
     browser.get(browser.baseUrl + url);
   });
+  // this.Given(/^I Click Place Badge of Postcards "([^"]*)"$/, function (placeBadge) {
+  //   var postcardPlaceBadge = browser.findElement(by.className('margin-r-5'));
+  //   expect(postcardPlaceBadge.getText()).toBe(placeBadge);
+  //   postcardPlaceBadge.click();
+  // });
+  //
+  // this.Given(/^I Click Search Button$/, function () {
+  //   element(By.css('div[style="max-width: 0px;"]'));
+  //   return;
+  // });
+  //
 
   this.When(/^I wait 10s$/, function () {
     browser.ignoreSynchronization = true;
     return browser.sleep(10000);
+  this.Then(/^should the title of the page be "([^"]*)"$/, function (expectedPageTitle) {
+    return browser.getTitle().then(function (title) {
+      assert.equal(title, expectedPageTitle, ' title is "' + title + '" but should be "' + expectedPageTitle);
+    });
+  });
 
   });
 
@@ -140,5 +156,80 @@ module.exports = function () {
   this.Then(/^should go to the page "([^"]*)"$/, function (url) {
     browser.get(browser.baseUrl + url);
   });
+  this.Then(/^should the title of the place be "([^"]*)"$/, function (expectedPlaceTitle) {
+    element(By.css(".navbar-top .name")).getText().then(function (title) {
+      assert.equal(title.trim(), expectedPlaceTitle, ' title is "' + title + '" but should be "' + expectedPlaceTitle);
+    });
+  });
+
+  this.Then(/^should the message be "([^"]*)"$/, function (expectedMessage) {
+    element(By.css(".toast-message")).getText().then(function (title) {
+      assert.equal(title.trim(), expectedMessage, ' title is "' + title + '" but should be "' + expectedMessage);
+    });
+  });
+//------------url selectors----------------//
+  this.Then(/^should the current url be "([^"]*)"$/, function (expectedUrl) {
+    var checkUrl = function (expectedUrl) {
+      return browser.getLocationAbsUrl()
+        .then(function(url){
+          if(url.indexOf(expectedUrl) > 0){
+            throw(url)
+          } else {
+            return url
+          }
+        })
+        .catch(function (url) {
+          return url
+        });
+    };
+    return browser.wait(checkUrl(expectedUrl), 15000).then(function (wrongUrl) {
+      assert.equal(wrongUrl.trim(), expectedUrl, ' title is "' + wrongUrl + '" but should be "' + expectedUrl);
+    });
+  });
+
+  this.Then(/^should the title be "([^"]*)"$/, function (expectedTitle) {
+    element(by.css('.testing-title')).getText().then(function (title) {
+      assert.equal(title.trim(), expectedTitle, ' title is "' + title + '" but should be "' + expectedTitle);
+    });
+  });
+
+
+  this.Then(/^should the title1 be "([^"]*)"$/, function (expectedTitle1) {
+    element(by.css('.testing-title1')).getText().then(function (title) {
+      assert.equal(title.trim(), expectedTitle1, ' title is "' + title + '" but should be "' + expectedTitle1);
+    });
+  });
+
+  this.Then(/^should see "([^"]*)" error message$/, function (expectedError) {
+    return element(By.css(".testing_err")).getText().then(function (error) {
+      return assert.equal(error.trim(), expectedError, ' error is "' + error + '" but should be "' + expectedError);
+    }).catch(function () {
+      return assert.ok(false, 'Can not find message!')
+    });
+  });
+
+  this.Then(/^the message have to be "([^"]*)"$/, function (expectedMessage) {
+    element(By.css(".toast-message .toast-success")).getText().then(function (no) {
+      assert.equal(no.trim(), expectedMessage, ' title is "' + no + '" but should be "' + expectedMessage);
+    });
+  });
+
+
+//---------------------attach files---------------------------------//
+
+  this.Given(/^I Attach dog-png$/, function () {
+
+    var path = require('path');
+    var process = require('process');
+
+    var fileToUpload = './e2e/assets/dog.png',
+      absolutePath = path.resolve(process.cwd(), fileToUpload);
+      console.log(absolutePath)
+    element(by.css('input[type="file"]')).sendKeys(absolutePath);
+
+
+  });
+
+//------------------------------------------------------------------//
 
 };
