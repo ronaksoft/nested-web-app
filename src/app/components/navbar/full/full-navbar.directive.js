918052303bf8e42da8cfd717881082b031e904c0
ali <me@robzizo.ir>
2016-10-23 10:30:41 +0330

    reverse navbar changes of 1 aban

--- src/app/components/navbar/full/full-navbar.directive.js
+++ src/app/components/navbar/full/full-navbar.directive.js
@@ -6,7 +6,7 @@
     .directive('nstNavbar', Navbar);
 
   /** @ngInject */
-  function Navbar($rootScope,$timeout) {
+  function Navbar($rootScope) {
     return {
       restrict: 'E',
       templateUrl: 'app/components/navbar/full/full-navbar.html',
@@ -20,58 +20,36 @@
         pictureClass : '@navPictureClass',
         title : '@navTitle',
         placeId : '@',
-        readyToShow : '=',
-        readyToAnime : '@'
+        readyToShow : '='
       },
-      link: function (scope, element, $attrs) {
-        scope.$watch(function () {
-          return $attrs.readyToAnime
-        },function () {
-          var ready = $attrs.readyToAnime;
-          availableNavAnim(ready)
-        });
-        function availableNavAnim(ready){
-
-          // create a scene
-
-          var controller = new ScrollMagic.Controller({
-            container: "body",
-            globalSceneOptions: {
-              triggerHook: "onLeave"
-            }
-          });
-          var tween,tween2,tween3,tween4,tween5,tween6;
-          tween = tween2 = tween3 = tween4 = tween5 = tween6  = new TimelineLite();
-
-          // create tween
-          if (element) tween = new TimelineLite()
-            .add(TweenLite.to($(element), 1, {css:{transform:'scaleY(0.5)'}, ease:Linear.easeNone}));
-
-          if ($(element).find('.reverse').length > 0) {
-            tween5 = new TimelineLite()
-              .add(TweenLite.to($('.reverse'), 1, {css:{transform:'scaleY(2)'}, ease:Linear.easeNone}));
-
-            tween6 = new TimelineLite()
-              .add(TweenLite.to($('.reverse-move'), 1, {css:{transform:'scaleY(2) translateY(-20px)'}, ease:Linear.easeNone}));
+      link: function (scope, element, attrs) {
+        // create a scene
+        var controller = new ScrollMagic.Controller({
+          container: "body",
+          globalSceneOptions: {
+            triggerHook: "onLeave"
           }
+        });
+        var tween,tween2,tween3,tween4;
+        tween = tween2 = tween3 = tween4 = new TimelineLite();
 
+        // create tween
+        if (element) tween = new TimelineLite()
+          .add(TweenLite.to($(element), 1, {css:{height:'88px'}, ease:Linear.easeNone}));
 
-          if ($(element).children().find( "h3" ).length > 0) { tween2 = new TimelineLite()
-            .add(TweenLite.to($(element).children().find( "h3" )[0], 1, {css:{color:'transparent'}, ease:Power4.easeOut}));}
+        if ($(element).children().find( "h3" ).length > 0) { tween2 = new TimelineLite()
+          .add(TweenLite.to($(element).children().find( "h3" )[0], 1, {css:{color:'transparent'}, ease:Power4.easeOut}));}
 
-          if ($("#content-plus").children().length > 0) {tween3 = new TimelineLite()
-            .add(TweenLite.to($("#content-plus"), 1, {css:{transform:'translateY(98px)'}, ease:Linear.easeNone}));}
+        if ($("#content-plus").children().length > 0) {tween3 = new TimelineLite()
+          .add(TweenLite.to($("#content-plus"), 1, {css:{transform:'translateY(98px)'}, ease:Linear.easeNone}));}
 
-          if ($("#new-post").length > 0) {tween4 = new TimelineLite()
-            .add(TweenLite.to($("#new-post"), 1, {css:{top:'128px'}, ease:Linear.easeNone}));}
+        if ($("#new-post").length > 0) {tween4 = new TimelineLite()
+          .add(TweenLite.to($("#new-post"), 1, {css:{top:'128px'}, ease:Linear.easeNone}));}
 
-          // build scene
-          var scene = new ScrollMagic.Scene({duration: 80, offset: 1})
-            .setTween([tween,tween2,tween3,tween4,tween5,tween6])
-            .addTo(controller);
-        }
-
-
+        // build scene
+        var scene = new ScrollMagic.Scene({duration: 80, offset: 1})
+          .setTween([tween,tween2,tween3,tween4])
+          .addTo(controller);
       }
     };
   }
