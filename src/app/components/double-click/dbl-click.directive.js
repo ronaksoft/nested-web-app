e56ec27905690570c0d4d8625522bba4f8e06148
robzizo <me@robzizo.ir>
2016-11-07 17:03:25 +0330

    double click on sidebar

--- src/app/components/double-click/dbl-click.directive.js
+++ src/app/components/double-click/dbl-click.directive.js
@@ -3,24 +3,28 @@
 
   angular
     .module('ronak.nested.web.components.sidebar')
-    .directive('dblClick', fn);
+    .directive(' ', fn);
 
   /** @ngInject */
-  function fn($state,$timeout,$location) {
+  function fn($timeout,$location) {
     return {
       restrict: 'A',
       link: function ($scope, $element, $attrs) {
-          var timer;
-        $element.click(function(event) {
-            $timeout.cancel(timer);
-            event.preventDefault();
-            timer = $timeout(function() {
-                $location.url($attrs.href.substring(1));
-            }, 200)
-        }); 
-        $element.dblclick(function(event) {
-            $timeout.cancel(timer);
-        }); 
+        var timer;
+        console.log($attrs.dblClick);
+        if ($attrs.dblClick == 'true') {
+            $element.click(function(event) {
+                $timeout.cancel(timer);
+                event.preventDefault();
+                timer = $timeout(function() {
+                    $location.url($attrs.href.substring(1));
+                }, 200)
+            }); 
+            $element.dblclick(function(event) {
+                $timeout.cancel(timer);
+            }); 
+        }
+    
       }
     };
   }
