38a6dd1930c72b0c0ab9154853917504c00141eb
ali <me@robzizo.ir>
2016-11-28 23:56:33 +0330

    modal opening animation like google ~ opens where mouse pressed

--- src/app/messages/post/post.modal.directive.js
+++ src/app/messages/post/post.modal.directive.js
@@ -4,7 +4,7 @@
     angular.module('ronak.nested.web.message')
       .directive('nstPostModal', PostModal);
 
-    function PostModal($uibModal, $state) {
+    function PostModal($timeout, $state, $window) {
       var directive = {
         link: link,
         template: function(element) {
@@ -19,13 +19,32 @@
         }
       };
 
-      function link(scope) {
+      function link(scope,$element) {
         scope.openPostModal = function (vmPost) {
           if (scope.post){
             scope.post.isRead = true;
           }
           $state.go('app.message', { postId : vmPost.id, model : vmPost }, { notify : false});
-        }
+        };
+
+        $element.click(function (event) {
+          var x = event.clientX/$window.innerWidth;
+          var y = event.clientY/$window.innerHeight;
+          console.log(x.toFixed(2),y.toFixed(2));
+
+          var style=document.createElement('style');
+          style.type='text/css';
+          if(style.styleSheet){
+            style.styleSheet.cssText='.modal {transform-origin: '+ x.toFixed(2) +'% ' + y.toFixed(2) + '%}';
+          }else{
+            style.appendChild(document.createTextNode('.modal {transform-origin: '+ event.clientX +'px ' + event.clientY + 'px}'));
+          }
+          document.getElementsByTagName('head')[0].appendChild(style);
+          $timeout(function () {
+            console.log('clicked');console.log('cars',$element,$('.modal-mlg'));
+            document.getElementsByTagName('head')[0].removeChild(style);
+          },4000)
+        })
       }
 
       return directive;
