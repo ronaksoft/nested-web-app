928d284ae49775c5ceba5fa59102215dc72df525
ali <me@robzizo.ir>
2016-12-04 12:15:47 +0330

    message user detail method

--- src/app/components/user/user-detail.controller.js
+++ src/app/components/user/user-detail.controller.js
@@ -18,6 +18,10 @@
       var query =  '@' + vm.username;
       var searchQury = new NstSearchQuery(query);
       $state.go('app.search', { search : NstSearchQuery.encode(searchQury.toString()) });
+    };
+
+    vm.messageUser = function () {
+      $state.go('app.compose', {placeId: vm.username});
     }
 
 
