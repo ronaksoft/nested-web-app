a3f49df71420e1b486d55d07df04190b27f55f13
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2016-12-21 11:53:07 +0330

    Track times in WS service and log the passed times

--- src/nested/server/ws.service.js
+++ src/nested/server/ws.service.js
@@ -5,7 +5,7 @@
     .factory('NstSvcWS', NstSvcWS);
 
   /**@Inject */
-  function NstSvcWS(NstSvcLogger, NstUtility) {
+  function NstSvcWS(NstSvcLogger, NstUtility, NST_WEBSOCKET_STATE, moment) {
 
     function WS(url, protocol) {
 
@@ -16,6 +16,11 @@
       this.onCloseQueue = [];
       this.onErrorQueue = [];
       this.onMessageQueue = [];
+
+      this.openTime = null;
+      this.lastSendTime = null;
+      this.lastReceiveTime = null;
+      this.lastCloseTime = null;
 
       this.initialize(this.url, this.protocol);
     }
@@ -40,23 +45,33 @@
 
     WS.prototype.send = function (data) {
       if (this.getState() === 1) {
+        this.lastSendTime = Date.now();
         this.socket.send(data);
       }
     };
 
     WS.prototype.close = function () {
-      this.socket.close();
+      if (this.getState() === NST_WEBSOCKET_STATE.CLOSING) {
+        NstSvcLogger.debug("The socket is being closed.");
+      } else if (this.getState() === NST_WEBSOCKET_STATE.CLOSED) {
+        NstSvcLogger.debug("The socket has already been closed.");
+      } else {
+        this.socket.close(1000, "The socked has ben closed intentially.");
+      }
     };
 
     WS.prototype.getState = function () {
-      return this.socket.readyState;
+      return this.socket.readyState
     }
 
     WS.prototype.reconnect = function () {
-      if (this.socket.readyState === 0 || this.socket.readyState === 1) { // Connecting or Open
-        this.socket.close();
-        this.initialize(this.url, this.protocol);
+      if (this.socket.readyState === NST_WEBSOCKET_STATE.CONNECTING || this.socket.readyState === NST_WEBSOCKET_STATE.OPEN) {
+        NstSvcLogger.debug("Trying to drop the open connection before reconnecting.");
+        this.close();
       }
+      var lastClose = this.lastCloseTime ? moment().diff(moment(this.lastCloseTime), "seconds") : "unknown";
+      NstSvcLogger.debug(NstUtility.string.format("Setting to establish a new connection to {0} after {1} sec.", this.url, lastClose));
+      this.initialize(this.url, this.protocol);
     };
 
     WS.prototype.initialize = function(url, protocol) {
@@ -67,27 +82,31 @@
       NstSvcLogger.debug(NstUtility.string.format("Establishing a new connection to {0}", url));
 
       this.socket.onopen = function (event) {
-        NstSvcLogger.debug("The socket connection has been established successfully.");
+        that.openTime = Date.now();
+        NstSvcLogger.debug("The connection has been established successfully.");
         _.forEach(that.onOpenQueue, function (action) {
           action(event);
         });
       };
 
       this.socket.onclose = function (event) {
-        NstSvcLogger.debug(NstUtility.string.format("The connection has been closed (code : {0}).", event.code));
+        that.lastCloseTime = Date.now();
+        var lastReceive = that.lastReceiveTime ? moment().diff(moment(that.lastReceiveTime), "seconds") : "unknown";
+        NstSvcLogger.debug(NstUtility.string.format("The connection has been closed (code : {0}, last receive : {1} sec).", event.code, lastReceive));
         _.forEach(that.onCloseQueue, function (action) {
           action(event);
         });
       };
 
-      this.socket.onerror = function (event) {
-        NstSvcLogger.debug(NstUtility.string.format("An error occured in socket connection ({0}).", event.message), event.error);
+      this.socket.onerror = function (event, error) {
+        NstSvcLogger.debug("An error occured in socket connection.", event.error);
         _.forEach(that.onErrorQueue, function (action) {
           action(event);
         });
       };
 
       this.socket.onmessage = function (event) {
+        that.lastReceiveTime = Date.now();
         _.forEach(that.onMessageQueue, function (action) {
           action(event);
         });
