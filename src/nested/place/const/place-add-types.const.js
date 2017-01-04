98dfbd2e40d3a7986029bca35c0b64a69dd5be58
Sina <e.hosseini.ir@gmail.com>
2017-01-04 17:53:03 +0330

    Change add personal command

--- src/nested/place/const/place-add-types.const.js
+++ src/nested/place/const/place-add-types.const.js
@@ -5,7 +5,7 @@
     .module('ronak.nested.web.models')
     .constant('NST_PLACE_ADD_TYPES', {
       ADD_GRAND_PLACE: 'ADD_GRAND_PLACE',
-      ADD_PERSONAL_PLACE: 'ADD_PERSONAL_PLACE',
+      ADD_PERSONAL_PLACE: 'ADD_PLACE',
       ADD_PLACE : 'ADD_PLACE'
     })
 })();
