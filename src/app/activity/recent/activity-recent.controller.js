311e3f9bf77b67b72757a4566bbabe81e28dec19
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-01-08 10:39:16 +0330

    Prevent requesting for activities in all places

--- src/app/activity/recent/activity-recent.controller.js
+++ src/app/activity/recent/activity-recent.controller.js
@@ -44,8 +44,6 @@
             return NstSvcLoader.inject(getRecentActivity(vm.settings));
           }
         });
-      } else {
-        return NstSvcLoader.inject(getRecentActivity(vm.settings));
       }
 
     })();
