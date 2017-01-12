2b664e84c8d19af76ed1e4bf602392859f06382f
ali <me@robzizo.ir>
2017-01-13 00:55:28 +0330

    post-card attachments

--- src/app/components/attachments/preview/attachments-preview-bar.directive.js
+++ src/app/components/attachments/preview/attachments-preview-bar.directive.js
@@ -5,7 +5,7 @@
     .module('ronak.nested.web.components.attachment')
     .directive('nstAttachmentsPreviewBar', AttachmentsPreviewBar);
 
-  function AttachmentsPreviewBar(NST_ATTACHMENTS_PREVIEW_BAR_MODE, NST_ATTACHMENTS_PREVIEW_BAR_ORDER) {
+  function AttachmentsPreviewBar(NST_ATTACHMENTS_PREVIEW_BAR_MODE, NST_ATTACHMENTS_PREVIEW_BAR_ORDER,$timeout) {
     return {
       restrict: 'E',
       templateUrl: 'app/components/attachments/preview/main.html',
@@ -16,7 +16,7 @@
       },
       link: function (scope,ele) {
         scope.internalMode = NST_ATTACHMENTS_PREVIEW_BAR_MODE.AUTO;
-
+        scope.overFlowLeft = scope.overFlowRight = false;
         scope.items = setOrder(scope.items, NST_ATTACHMENTS_PREVIEW_BAR_ORDER.order);
 
         if (modeIsValid(scope.mode)) {
@@ -59,14 +59,82 @@
           scope.flexOneWidth = scale * (unkHeight * imgOneRatio);
           scope.flexTwoWidth = scale * (unkHeight * imgTwoRatio);
         }
+
         scope.onClick = function (item) {
           if (scope.onItemClick) {
             scope.onItemClick(item, scope.items);
           }
         };
+
+
+        $timeout( function () {
+          checkScroll(ele[0].children[1]);
+        },1000 );
+
+        function checkScroll(el) {
+          scope.scrollWrp = el;
+          if (el.clientWidth < el.scrollWidth && el.scrollLeft == 0) {
+            scope.overFlowRight = true;
+            scope.overFlowLeft = false;
+          } else if(el.clientWidth + el.scrollLeft == el.scrollWidth && el.clientWidth < el.scrollWidth) {
+            scope.overFlowRight = false;
+            scope.overFlowLeft = true;
+          } else if ( el.clientWidth < el.scrollWidth ) {
+            scope.overFlowRight = true;
+            scope.overFlowLeft = true;
+          }
+        }
+
+
+        scope.goLeft = function (e) {
+          e.stopPropagation();
+          var k = makeid();
+          var count =  {};
+          count[k] = 0;
+          scrollLeft(count[k]);
+        };
+        function scrollLeft(count) {
+          var el = scope.scrollWrp;
+          ++count;
+          if( count < 70 ) { $timeout (function () {
+            el.scrollLeft -= 1;
+            scrollLeft(count);
+          },1)
+          } else {
+            checkScroll(el)
+          }
+        }
+
+        scope.goRight = function (e) {
+          e.stopPropagation();
+          var k = makeid();
+          var count =  {};
+          count[k] = 0;
+          scrollRight(count[k]);
+        };
+        function scrollRight(count) {
+          var el = scope.scrollWrp;
+          ++count;
+          if( count < 70 ) { $timeout (function () {
+            el.scrollLeft += 1;
+            scrollRight(count);
+          },1)
+          } else {
+            checkScroll(el)
+          }
+        }
+
       }
     };
+    function makeid() {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+      for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
+    }
     function modeIsValid(mode) {
       return _.values(NST_ATTACHMENTS_PREVIEW_BAR_MODE).indexOf(mode) > -1;
     }
