21e56239789b52f1544e44bd6deb5be173d93cce
robzizo <me@robzizo.ir>
2017-01-22 16:12:30 +0330

    new compose page

--- src/app/pages/compose/compose.controller.js
+++ src/app/pages/compose/compose.controller.js
@@ -65,6 +65,25 @@
       ]
     };
 
+    $scope.editorOptions = {
+      language: 'en',
+      extraPlugins: 'sharedspace,font,language,bidi,justify,colorbutton',
+      sharedSpaces: {
+        top: 'editor-btn',
+        bottom: 'editor-txt'
+      },
+      toolbar: [
+        ["FontSize","Font"],
+        ["Bold","Italic","Underline"],
+        ["JustifyLeft","TextColor","BidiRtl","BidiLtr"]
+      ],
+      fontSize_sizes: '16/16px;24/24px;48/48px;',
+      colorButton_colors: 'CF5D4E,454545,FFF,CCC,DDD,CCEAEE,66AB16',
+      // Remove the redundant buttons from toolbar groups defined above.
+      //removeButtons: 'Strike,Subscript,Superscript,Anchor,Specialchar',
+      removePlugins: 'resize,elementspath'
+    };
+
     vm.configs = {
       tinymce: {
         inline: false,
