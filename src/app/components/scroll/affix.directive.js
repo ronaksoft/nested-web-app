202346935dc615787607c5843049fcbbd63ed7c7
robzizo <me@robzizo.ir>
2017-01-27 16:28:47 +0330

    quick msg RTL

--- src/app/components/scroll/affix.directive.js
+++ src/app/components/scroll/affix.directive.js
@@ -111,7 +111,8 @@
             if (!!$attrs.firstImp ) {
               $element.css('position', 'fixed');
               $element.css('top', parseInt(top) - parseInt(topOffset) + 'px');
-              $element.css('left', offLeft + 'px');
+              if (isRTL == 'ltr')$element.css('left', offLeft + 'px');
+              if (isRTL == 'rtl')$element.css('right', offLeft + 'px');
               $element.css('width', width + 'px');
               $element.css('height', height + 'px');
               if(clearRight) {
