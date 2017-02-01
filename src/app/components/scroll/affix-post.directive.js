25fb9a89f1df36e9552a48a3724692c214b1afdb
robzizo <me@robzizo.ir>
2017-02-01 13:51:25 +0330

    post card controls in mobile view fix ( both rtl ltr )

--- src/app/components/scroll/affix-post.directive.js
+++ src/app/components/scroll/affix-post.directive.js
@@ -6,22 +6,25 @@
     .directive('affixPost', onScroll);
 
   /** @ngInject */
-  function onScroll($window,$rootScope,$timeout,NstSvcTranslation) {
+  function onScroll($window,$rootScope,deviceDetector,$timeout,NstSvcTranslation) {
     return {
       restrict: 'A',
       link: function ($scope, $element, $attrs) {
         var win = angular.element($window);
+        var isMobile = deviceDetector.isMobile() || deviceDetector.isTablet();
         var isRTL = $rootScope._direction == 'rtl';
+        var MobTopOff = isMobile ? 56 : 0;
         win.bind('scroll', affixElement);
 
         //TODO handle win resize event
         function affixElement() {
 
-          if ($window.pageYOffset > $element.parent().offset().top && $window.pageYOffset < $element.parent().children().first().height() + $element.parent().offset().top - 50) {
+          if ($window.pageYOffset + MobTopOff > $element.parent().offset().top && $window.pageYOffset < $element.parent().children().first().height() + $element.parent().offset().top - 50) {
             $element.css('position', 'fixed');
-            $element.css('top', 24 + 'px');
+            $element.css('top', 24 + MobTopOff + 'px');
             if (!isRTL) $element.css('left', $element.parent().offset().left + 'px');
-            if (isRTL) $element.css('right', $element.parent().offset().left - 20 + 'px');
+            if (isRTL && !isMobile) $element.css('right', $element.parent().offset().left - 20 + 'px');
+            if (isRTL && isMobile) $element.css('right', $element.parent().offset().left + 'px');
           } else {
             $element.css('position', '');
             $element.css('top', '');
