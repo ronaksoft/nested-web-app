4747c9618976fbc5ac76f429042996cd0dd9fa24
Sina <e.hosseini.ir@gmail.com>
2017-02-01 17:33:29 +0330

    clean code

--- src/app/components/avatar-filter.js
+++ src/app/components/avatar-filter.js
@@ -3,9 +3,10 @@
 
   angular
     .module('ronak.nested.web.components')
-    .filter('avatar', function(moment) {
+    .filter('avatar', function() {
 
       return function(model, size) {
+
         size = size || "x32";
         if (model && model.hasPicture()) {
           return model.picture.getUrl(size);
