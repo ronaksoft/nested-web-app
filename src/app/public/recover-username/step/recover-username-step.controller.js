2078487e0a4f196c38821026e393cda6c012df23
Sina <e.hosseini.ir@gmail.com>
2017-02-02 17:34:23 +0330

    show username

--- src/app/public/recover-username/step/recover-username-step.controller.js
+++ src/app/public/recover-username/step/recover-username-step.controller.js
@@ -28,7 +28,9 @@
           'vid': verificationId,
         }
       }).post().then(function(result) {
-        if (result.data.status === "ok") {
+        console.log(result)
+        if (result.status === "ok") {
+          vm.username = result.data.uid;
           deferred.resolve(result.data);
         } else {
           deferred.reject('unknown');
