055ce2e94eea22171b192a50aee1d1e8b11e6aa0
robzizo <me@robzizo.ir>
2017-02-09 12:14:05 +0330

    sidebar auto tooltip on ellipsis name

--- src/app/components/sidebar/ellipsis.directive.js
+++ src/app/components/sidebar/ellipsis.directive.js
@@ -4,19 +4,19 @@
   angular.module('ronak.nested.web.components.text')
     .directive('ellipsis', ellipsis);
 
-  function ellipsis() {
+  function ellipsis($timeout) {
     var directive = {
       link: link,
-      template: function(element, attrs) {
-        var tag = element[0].nodeName;
-        return '<' + tag + ' ng-mouseenter="ellipsisControl()" data-uib-tooltip="{{name}}" data-tooltip-enable="{{tooltipEnabled}}" data-tooltip-append-to-body="true" data-tooltip-placement="top auto" ng-transclude></' + tag +'>';
-      },
-      restrict: 'A',
-      scope: {
-        name: '=ellipsis'
-      },
-      replace: true,
-      transclude: true
+      // template: function(element, attrs) {
+      //   var tag = element[0].nodeName;
+      //   return '<' + tag + ' ng-mouseenter="ellipsisControl()" data-uib-tooltip="{{name}}" data-tooltip-enable="{{tooltipEnabled}}" data-tooltip-append-to-body="true" data-tooltip-placement="top auto" ng-transclude></' + tag +'>';
+      // },
+      restrict: 'A'
+      // scope: {
+      //   name: '=ellipsis'
+      // },
+      // replace: true,
+      // transclude: true
     };
 
     function link(scope, element, attrs) {
@@ -25,11 +25,13 @@
        *
        * @param  {type} places list of places to be shown
        */
-      scope.ellipsisControl = function () {
+      scope.$parent.place.forceTooltip = false;
+
+      $timeout(function () {
         if (element[0].clientWidth < element[0].scrollWidth) {
-          scope.tooltipEnabled = true;
+          scope.$parent.place.forceTooltip = true;
         }
-      }
+      },100);
     }
 
     return directive;
