e52d390eb99e73b78b9c399d8c17715040605267
robzizo <me@robzizo.ir>
2017-02-17 14:32:29 +0330

    translate tag

--- src/app/account/crop/crop.controller.js
+++ src/app/account/crop/crop.controller.js
@@ -76,8 +76,7 @@
           ia[i] = byteString.charCodeAt(i);
         }
 
-
-        return new Blob([ia], {type: mimeString});
+        return new File([ia], filename + '.' +  mimeString.split('/')[1], {type: mimeString});
       }
 
     })
