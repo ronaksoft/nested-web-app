5138f353a4634a3b4a451c58f3b2dff6582be41e
robzizo <me@robzizo.ir>
2017-02-21 16:18:45 +0330

    navbar mobile view issue

---
+++ src/app/place/partials/view/place-head.directive.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ronak.nested.web.components.attachment')
+    .directive('placeHead', placeHead);
+
+  function placeHead() {
+    return {
+      restrict: 'E',
+      scope: {
+        pict : '=',
+        env : '='
+      },
+      replace: true,
+      templateUrl : 'app/place/partials/view/place-head.html',
+      link: function (scope,ele,att) {
+
+        scope.settings = scope.env == 'settings';
+
+      }
+    };
+
+  }
+})();
