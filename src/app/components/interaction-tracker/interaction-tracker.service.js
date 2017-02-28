8ffd3315a14a269a328aeb80818cbd91516d9306
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-02-28 14:47:22 +0330

    Add user id to google aalytics records

--- src/app/components/interaction-tracker/interaction-tracker.service.js
+++ src/app/components/interaction-tracker/interaction-tracker.service.js
@@ -6,9 +6,11 @@
     .service('NstSvcInteractionTracker', NstSvcInteractionTracker);
 
   /** @ngInject */
-  function NstSvcInteractionTracker(Analytics, NST_CONFIG, $state) {
+  function NstSvcInteractionTracker(Analytics, NST_CONFIG, $state, NstSvcAuth) {
     function InteractionTracker() {
-
+      if (NstSvcAuth && NstSvcAuth.user) {
+        Analytics.set('&uid', NstSvcAuth.user.id);
+      }
     }
 
     InteractionTracker.prototype.trackEvent = function (category, action, value) {
