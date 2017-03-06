0f9bed213222b54f20149e06956bccdf63e8465e
Sina <e.hosseini.ir@gmail.com>
2017-03-06 12:56:21 +0330

    handle push notification click event

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -8,8 +8,8 @@
   /** @ngInject */
   function AppController($q, $scope, $window, $rootScope, $timeout, $state, $stateParams, $uibModalStack, $interval, $log, $injector,
                          hotkeys, deviceDetector, NstSvcInteractionTracker,
-                         NST_CONFIG, NST_UNREGISTER_REASON, NST_PUBLIC_STATE, NST_DEFAULT, NST_PAGE, NST_SRV_ERROR, NST_AUTH_EVENT, NST_SRV_EVENT, NST_PLACE_ACCESS,
-                         NstSvcServer, NstSvcAuth, NstFactoryError, NstSvcLogger, NstSvcModal, NstSvcI18n, NstSvcNotification,
+                         NST_DEFAULT, NST_AUTH_EVENT, NST_SRV_EVENT, NST_NOTIFICATION_FACTORY_EVENT,
+                         NstSvcServer, NstSvcAuth, NstSvcLogger, NstSvcI18n, NstSvcNotification, NstSvcNotificationFactory,
                          NstObject) {
     var vm = this;
 
@@ -44,16 +44,16 @@
       vm.showLoadingScreen = true;
     });
     $scope.$on('collapse-sidebar', function () {
-      vm.viewSettings.sidebar.collapsed =! vm.viewSettings.sidebar.collapsed
+      vm.viewSettings.sidebar.collapsed = !vm.viewSettings.sidebar.collapsed
     });
 
     $scope.$watch(function () {
       return vm.viewSettings.sidebar.collapsed
-    },function () {
-      var tooltip  = $('body').find('.tooltip');
-      if ( tooltip.is(":visible")){
+    }, function () {
+      var tooltip = $('body').find('.tooltip');
+      if (tooltip.is(":visible")) {
         tooltip.first().hide()
-      } else  {
+      } else {
         tooltip.first().show()
 
       }
@@ -121,7 +121,7 @@
         if (event) {
           event.preventDefault();
         }
-        $state.go('public.signin-back', { back : angular.toJson({ name : state.name, params : stateParams }) });
+        $state.go('public.signin-back', {back: angular.toJson({name: state.name, params: stateParams})});
       }
     }
 
@@ -138,17 +138,17 @@
 
       // return the default state if could not find any primary route
       return {
-        default : true,
-        state : $state.get(NST_DEFAULT.STATE),
-        params : {}
+        default: true,
+        state: $state.get(NST_DEFAULT.STATE),
+        params: {}
       };
     }
 
     $rootScope.goToLastState = function (disableNotify, defaultState) {
       var previous = defaultState || restoreLastState();
 
-      if (disableNotify && !previous.default){
-        $state.go(previous.state.name, previous.params, {notify : false});
+      if (disableNotify && !previous.default) {
+        $state.go(previous.state.name, previous.params, {notify: false});
       } else {
         $state.go(previous.state.name, previous.params);
       }
@@ -161,8 +161,8 @@
     });
 
     //Handle all mailto links
-    $(window).on('click', function(event) {
-      if(!$(event.target).is('a[href^="mailto"]')) {
+    $(window).on('click', function (event) {
+      if (!$(event.target).is('a[href^="mailto"]')) {
         return;
       }
 
@@ -170,11 +170,11 @@
       //TODO:: check domain base on config
       if (
         addr.split('@')[1] &&
-        (addr.split('@')[1] === 'nested.me' || addr.split('@')[1] === 'nested.ronaksoftware.com')){
+        (addr.split('@')[1] === 'nested.me' || addr.split('@')[1] === 'nested.ronaksoftware.com')) {
         addr = addr.split('@')[0];
       }
 
-      $state.go('app.place-compose',{placeId : addr}, {notify : false});
+      $state.go('app.place-compose', {placeId: addr}, {notify: false});
       // Both are needed to avoid triggering other event handlers
       event.stopPropagation();
       event.preventDefault();
@@ -183,6 +183,31 @@
     function trackBehaviour(category, behaviour, value) {
       NstSvcInteractionTracker.trackEvent(category, behaviour, value);
     }
+
+    // Listen to notification behavior
+
+    function viewPost(postId) {
+      $state.go('app.message', {postId: postId}, {notify: false});
+    }
+
+    function openPlace(placeId) {
+      $state.go('app.place-messages', {placeId: placeId});
+    }
+
+
+    //Receive and handle new external push notification after user click on desktop notification
+    NstSvcNotification.addEventListener(NST_NOTIFICATION_FACTORY_EVENT.EXTERNAL_PUSH_ACTION, function (event) {
+      switch (event.detail.action) {
+        case NST_NOTIFICATION_FACTORY_EVENT.OPEN_PLACE:
+          openPlace(event.detail.placeId);
+          break;
+        case NST_NOTIFICATION_FACTORY_EVENT.OPEN_POST_VIEW:
+          viewPost(event.detail.postId);
+          break;
+      }
+      NstSvcNotificationFactory.markAsSeen(event.detail.notificationId)
+    })
+
 
   }
 })();
--- src/nested/notification/notification-factory-event.const.js
+++ src/nested/notification/notification-factory-event.const.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -6,6 +6,9 @@
     .constant('NST_NOTIFICATION_FACTORY_EVENT', {
       UPDATE: 'notifications-count-change',
       NEW_NOTIFICATION: 'new-notification',
-      OPEN_INVITATION_MODAL : 'open-invitation-modal'
+      OPEN_INVITATION_MODAL: 'open-invitation-modal',
+      OPEN_POST_VIEW: 'open-post-view',
+      OPEN_PLACE: 'open-place',
+      EXTERNAL_PUSH_ACTION: 'external-push-action'
     });
 })();
