996ef22640b46dcd0a91c81bef9274ad64f45f51
robzizo <me@robzizo.ir>
2017-03-13 12:16:48 +0330

    navbar bugfix

--- src/app/components/scroll/hide-tips-on-scroll.directive.js
+++ src/app/components/scroll/hide-tips-on-scroll.directive.js
@@ -10,22 +10,20 @@
     return {
       link: function ($scope, $element) {
 
+        var scrollPos = 0,scrollOff = 220;
         $(window).scroll(function(e){
-
 
           //hide tips
           dissappear($element.find('.tooltip'));
           dissappear($element.find('.popover-userdetail'));
 
-          // if(e.currentTarget.pageYOffset > scrollOff && e.currentTarget.pageYOffset < scrollPos){
-          //   $('body').addClass('scrolled')
-          // } else if (e.currentTarget.pageYOffset < scrollOff || ( e.currentTarget.pageYOffset > scrollOff && $('body').hasClass('scrolled') ) ) {
-          //   $('body').removeClass('scrolled')
-          // }
+          if(e.currentTarget.pageYOffset > scrollOff && e.currentTarget.pageYOffset < scrollPos){
+            $('body').addClass('scrolled')
+          } else if (e.currentTarget.pageYOffset < scrollOff || ( e.currentTarget.pageYOffset > scrollOff && $('body').hasClass('scrolled') ) ) {
+            $('body').removeClass('scrolled')
+          }
 
-          // scrollPos = e.currentTarget.pageYOffset;
-
-
+          scrollPos = e.currentTarget.pageYOffset;
 
         });
 
