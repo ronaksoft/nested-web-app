1790f1914f7124760a56a291940e0bf354258ecf
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-03-15 15:48:31 +0330

    Clean notification.permission.const.js

--- src/nested/notification/notification.permission.const.js
+++ src/nested/notification/notification.permission.const.js
@@ -4,6 +4,6 @@
   angular.module('ronak.nested.web.components.notification').constant('NST_PERMISSION_NOTIFICATION', {
     DEFAULT: 'default',
     GRANTED: 'granted',
-    DENIED: 'denied',
+    DENIED: 'denied'
   });
 })();
