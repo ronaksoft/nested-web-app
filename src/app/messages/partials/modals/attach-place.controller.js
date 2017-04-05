f9d8d6839c9de449e631787c5ebc759721a0650a
robzizo <me@robzizo.ir>
2017-04-05 12:20:23 +0430

    early stage of attach place

--- src/app/messages/partials/modals/attach-place.controller.js
+++ src/app/messages/partials/modals/attach-place.controller.js
@@ -6,9 +6,11 @@
     .controller('AttachPlaceController', AttachPlaceController);
 
   function AttachPlaceController($timeout, $scope, $q,
+                                 NstSvcTranslation,
                                    moment, toastr, _) {
     var vm = this;
-    
+    vm.inputPlaceHolderLabel = NstSvcTranslation.get("Enter a Place name or a Nested address...");
+
   }
 
 })();
