2aa29fa44c21a917008e6302e8b7bd01e3cec376
Ali <me@robzizo.ir>
2017-04-16 15:14:27 +0430

    i

---
+++ src/app/components/text/ellipsis-mail.directive.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ronak.nested.web.components')
+    .directive('ellipsisMail', ellipsisMail);
+
+  function ellipsisMail($timeout) {
+    return {
+      restrict: 'A',
+      link: function (scope ,$element, attrs) {
+
+        function resizer() {
+
+          var maxChars = 36;
+          var str = $element.text();
+          var $numWords = str.length || 0;
+          var atSignIndex = str.indexOf("@");
+          var domainLength = $numWords - atSignIndex;
+
+
+          if ($numWords > maxChars ) {
+            scope.$parent.forceTooltip = true;
+
+            if(domainLength < 28){
+              str = str.substr(0, maxChars - domainLength - 7) + '...' + str.substr(atSignIndex - 4, str.length);
+            } else {
+              str = str.substr(0, (maxChars / 2) - 4) + '...' + str.substr(atSignIndex - 4, maxChars / 4) + '...' + str.substr($numWords - (maxChars / 4), $numWords);
+            }
+
+            $element.text(str);
+          }
+        }
+
+        scope.$watch(function () {
+
+          return $element.text().length
+        },function () {
+          resizer()
+        })
+
+
+
+      }
+    };
+  }
+})();
