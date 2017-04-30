00dbbc676120924edeb67b3e5927ea287fcd0373
Ali <me@robzizo.ir>
2017-04-30 18:24:11 +0430

    remove log

--- src/app/components/scroll/scroll-spy.directive.js
+++ src/app/components/scroll/scroll-spy.directive.js
@@ -9,7 +9,6 @@
             link: function(scope, elem, attr) {
                 var offset = parseInt(attr.scrollOffset, 10)
                 if(!offset) offset = 10;
-                console.log("offset:  " + offset);
                 elem.scrollspy({ "offset" : offset});
                 scope.$watch(attr.scrollSpy, function(value) {
                     $timeout(function() { 
