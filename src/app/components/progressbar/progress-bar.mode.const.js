303e681c4530ea5cbbb689925b9b50f3ac3f63f9
Sina <e.hosseini.ir@gmail.com>
2017-05-10 11:17:53 +0430

    revert progressbar directive

---
+++ src/app/components/progressbar/progress-bar.mode.const.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ronak.nested.web.components')
+    .constant('NST_PROGRESSBAR_MODE', {
+      CIRCLE: 'Circle',
+      LINE: 'Line'
+    });
+})();
