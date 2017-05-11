bb309efbda0afcfad59d50949dc6a7781111eaa2
Ali <me@robzizo.ir>
2017-05-11 11:30:29 +0430

    bugfix in analysing document mimetypes

--- src/nested/file/file-type.service.js
+++ src/nested/file/file-type.service.js
@@ -20,7 +20,12 @@
       'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
       'application/vnd.ms-powerpoint',
       'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-      'application/vnd.openxmlformats-officedocument.presentationml.slideshow'
+      'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'application/vnd.ms-excel.sheet.macroEnabled.12',
+      'application/vnd.ms-excel.template.macroEnabled.12',
+      'application/vnd.ms-excel.addin.macroEnabled.12',
+      'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
     ];
 
     fileGroups[NST_FILE_TYPE.IMAGE] = [
