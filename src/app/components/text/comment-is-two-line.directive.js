5392c0d7455c0629fe8e2df4d382db9bf8055976
Ali <me@robzizo.ir>
2017-05-25 13:39:15 +0430

    multi line comment support in farsi

--- src/app/components/text/comment-is-two-line.directive.js
+++ src/app/components/text/comment-is-two-line.directive.js
@@ -13,11 +13,7 @@
         function resizer() {
           if ($element.height() > 17) {
             $element.width('100%');
-            if ( $rootScope._direction === 'rtl') {
-              $element.css({'display' : 'flex','justify-content' : 'flex-end'});
-            } else {
-              $element.css({'display' : 'flex','justify-content' : 'flex-start'});
-            }
+            $element.css({'display' : 'flex','justify-content' : 'flex-start'});
           }
         }
 
