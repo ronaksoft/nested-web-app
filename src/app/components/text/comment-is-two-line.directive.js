0632b3b6f122c6a64c6f1050fe5632ff0ae384d5
Ali <me@robzizo.ir>
2017-05-25 13:25:12 +0430

    RTL new navbar fix

--- src/app/components/text/comment-is-two-line.directive.js
+++ src/app/components/text/comment-is-two-line.directive.js
@@ -5,7 +5,7 @@
     .module('ronak.nested.web.components')
     .directive('isTwoLine', isTwoLine);
 
-  function isTwoLine($timeout) {
+  function isTwoLine($timeout, $rootScope) {
     return {
       restrict: 'A',
       link: function (scope ,$element, attrs) {
@@ -13,6 +13,11 @@
         function resizer() {
           if ($element.height() > 17) {
             $element.width('100%');
+            if ( $rootScope._direction === 'rtl') {
+              $element.css({'display' : 'flex','justify-content' : 'flex-end'});
+            } else {
+              $element.css({'display' : 'flex','justify-content' : 'flex-start'});
+            }
           }
         }
 
