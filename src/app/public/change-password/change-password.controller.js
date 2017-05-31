df4807449be12ca6712a032d36db40945cf5f026
Sina <e.hosseini.ir@gmail.com>
2017-05-31 18:17:44 +0430

    force change pass page

---
+++ src/app/public/change-password/change-password.controller.js
@@ -0,0 +1,65 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ronak.nested.web.user')
+    .controller('ForceChangePasswordController', RecoverPasswordController);
+
+  /** @ngInject */
+  function RecoverPasswordController($scope, $log, $state,
+                                     toastr,
+                                     NstSvcUserFactory, NstSvcAuth,
+                                     NST_SRV_ERROR, NST_NAVBAR_CONTROL_TYPE, NstSvcTranslation) {
+    var vm = this;
+    vm.signout = signout;
+    vm.user = NstSvcUserFactory.currentUser;
+    vm.ready = true;
+    vm.model = {
+      oldPassword: '',
+      newPassword: '',
+      newPasswordConfirm: ''
+    };
+
+    vm.change = change;
+
+    function change(isValid) {
+      vm.submitted = true;
+      if (isValid) {
+        if (vm.model.newPassword.length < 6) {
+          toastr.error(NstSvcTranslation.get('Your new password must be between 6 characters.'));
+          return;
+        }
+        NstSvcUserFactory.changePassword(vm.model.oldPassword, vm.model.newPassword).then(function (result) {
+          toastr.success(NstSvcTranslation.get('You have changed your password successfully.'));
+          vm.model = {
+            oldPassword: '',
+            newPassword: '',
+            newPasswordConfirm: ''
+          };
+          vm.submitted = false;
+
+          $state.go('app.messages-favorites');
+
+        }).catch(function (error) {
+          console.log(error);
+          if (error.code === NST_SRV_ERROR.INVALID) {
+            return toastr.error(NstSvcTranslation.get('The old password you have entered is incorrect'));
+          } else {
+            return toastr.error(NstSvcTranslation.get('An error has occured while trying to chaing your password'));
+          }
+
+        });
+      }
+    }
+
+
+    function signout() {
+
+      NstSvcAuth.logout();
+      $state.go('public.signin');
+
+    }
+
+
+  }
+})();
