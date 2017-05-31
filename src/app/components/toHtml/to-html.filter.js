d2aae93b13b43243d5cc6e013cfb530c9108b57b
Sina <e.hosseini.ir@gmail.com>
2017-05-31 15:37:14 +0430

    fix comment filters

--- src/app/components/toHtml/to-html.filter.js
+++ src/app/components/toHtml/to-html.filter.js
@@ -7,11 +7,10 @@
       return function (content) {
 
         return ((content || "") + "")  // make sure it is a string;
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
+        // .replace(/&/g, "&amp;")
+        // .replace(/</g, "&lt;")
+        // .replace(/>/g, "&gt;")
         .replace(/\t/g, "    ")
-        .replace(/ /g, "&#8203;&nbsp;&#8203;")
         .replace(/\r\n|\r|\n/g, "<br />");
 
       };
