30fb668e1670177606a2fa3d683694ef6d9020f5
Sina <e.hosseini.ir@gmail.com>
2017-06-11 13:53:05 +0430

    check hidden comment access

--- src/app/messages/comments-board.controller.js
+++ src/app/messages/comments-board.controller.js
@@ -130,9 +130,8 @@
     }
 
     function allowToRemoveComment(comment) {
-      return vm.hasCommentRemoveAccess || (comment.sender.id === vm.user.id &&
-        (Date.now() - comment.timestamp) < 24 * 60 * 60 * 1e3);
-
+      return vm.hasCommentRemoveAccess ||
+        (comment.sender.id === vm.user.id && ((Date.now() - comment.timestamp) < 24 * 60 * 60 * 1e3));
     }
 
     /**
