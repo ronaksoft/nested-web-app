53afcf847480a09b998d9cf2740738554143cbda
Ali <me@robzizo.ir>
2017-06-14 18:07:10 +0430

    new sidebar UI

--- src/app/components/sidebar/sidebar.controller.js
+++ src/app/components/sidebar/sidebar.controller.js
@@ -8,7 +8,7 @@
     /** @ngInject */
     function SidebarController($q, $scope, $state, $stateParams, $uibModal, $window, $rootScope, $timeout,
                                _,
-                               NST_DEFAULT, NST_AUTH_EVENT, NST_INVITATION_FACTORY_EVENT, NST_PLACE_FACTORY_EVENT, NST_CONFIG,NST_KEY,
+                               NST_DEFAULT, NST_AUTH_EVENT, NST_INVITATION_FACTORY_EVENT, NST_PLACE_FACTORY_EVENT, NST_CONFIG,NST_KEY, deviceDetector,
                                NST_EVENT_ACTION, NST_USER_FACTORY_EVENT, NST_POST_FACTORY_EVENT, NST_NOTIFICATION_FACTORY_EVENT, NST_SRV_EVENT, NST_NOTIFICATION_TYPE,
                                NstSvcAuth, NstSvcServer, NstSvcLogger, NstSvcNotification, NstSvcTranslation,
                                NstSvcPostFactory, NstSvcPlaceFactory, NstSvcInvitationFactory, NstUtility, NstSvcUserFactory, NstSvcSidebar, NstSvcNotificationFactory,
@@ -563,7 +563,7 @@
                 totalUnread += obj.count;
               });
               vm.totalUnreadPosts = totalUnread;
-              vm.insertItems();
+              if ( deviceDetector.isDesktop() ) vm.insertItems();
               $rootScope.$emit('unseen-activity-notify', totalUnread);
               $rootScope.$broadcast('init-controls-sidebar');
             });
