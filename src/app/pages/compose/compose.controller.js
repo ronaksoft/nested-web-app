c5eeb105760f4070904ae9eeb7d4eec22a904d17
Ali <me@robzizo.ir>
2017-06-15 13:57:52 +0430

    emoji support windows 7

--- src/app/pages/compose/compose.controller.js
+++ src/app/pages/compose/compose.controller.js
@@ -1011,21 +1011,17 @@
           if ( vm.quickMode ) return
           var el = editor.selection.element();
           if (el && je.which === 91) {
-            // console.log('cmdPress');
             vm.cmdPress = true;
           }
           if (el && je.which === 86 && vm.cmdPress) {
             vm.cmdVPress = true;
-            // console.log('cmdVPress');
             el.scrollIntoView({block: "end", behavior: "smooth"});
           }
         },
         'froalaEditor.keyup': function (e, editor, je) {
           if ( vm.quickMode ) return
-          console.log(editor.popups);
           var el = editor.selection.element();
           if (el && (je.which === 13 || vm.cmdVPress)) {
-            // console.log('cmdVPress up');
             el.scrollIntoView({block: "end", behavior: "smooth"});
           }
           vm.cmdPress = false;
