a9dccbf431826b367f6d3952906b3b2e890ed901
Ehsan Hosseini <e.hosseini.ir@gmail.com>
2017-08-01 23:28:59 +0430

    config web-local-server 2

---
+++ lws.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rewrite: [
+    {
+      "from": "/getConfig/*",
+      "to": "http://localhost:5000/$1"
+    },
+    {
+      "from": "/m/*",
+      "to": "http://localhost:8889/$1"
+    }
+  ]
+};
