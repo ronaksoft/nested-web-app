40d4d6cffb48826ab861938c3af72b3bf0ac751e
Ali mahmoodi <robzizo@Apples-iMac.local>
2017-08-06 11:28:25 +0430

    final ui retouchs + some triggers on buttons

--- src/app/label/manage-label.controller.js
+++ src/app/label/manage-label.controller.js
@@ -5,20 +5,24 @@
     .module('ronak.nested.web.components')
     .controller('manageLabelController', manageLabelController);
 
-  function manageLabelController($timeout, $scope, $q, $uibModalInstance,
+  function manageLabelController($timeout, $scope, $q, $uibModalInstance, $uibModal,
     moment, toastr, _,
     NstSvcPostFactory, NstSvcPlaceFactory, NstSvcTranslation, NstUtility,
     NstTinyPlace) {
 
     var vm = this;
-    vm.labelManager = false;
-    // $uibModal.open({
-    //     animation: false,
-    //     size: 'lg-white multiple',
-    //     templateUrl: 'app/label/partials/edit-label.html',
-    //     controller: 'editLabelController',
-    //     controllerAs: 'ctrl'
-    // })
+    vm.labelManager = true;
+    vm.editLabel = editLabel
+    
+    function editLabel(){
+      $uibModal.open({
+        animation: false,
+        size: 'lg-white multiple',
+        templateUrl: 'app/label/partials/edit-label.html',
+        controller: 'editLabelController',
+        controllerAs: 'ctrl'
+      })
+    }
   }
 
 })();
