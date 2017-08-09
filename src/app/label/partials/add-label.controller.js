47cda2cc88ea45cd36a7ba22b5d73070a975bf26
Ali mahmoodi <robzizo@Apples-iMac.local>
2017-08-09 12:38:16 +0430

    add - label pagination

--- src/app/label/partials/add-label.controller.js
+++ src/app/label/partials/add-label.controller.js
@@ -6,40 +6,74 @@
     .controller('addLabelController', addLabelController);
 
   function addLabelController($timeout, $scope, $q, $uibModalInstance,
-    moment, toastr, _, NstSvcLabelFactory,
+    moment, toastr, _, NstSvcLabelFactory, NST_LABEL_SEARCH_FILTER,
     NstSvcPostFactory, NstSvcPlaceFactory, NstSvcTranslation, NstUtility,
     NstTinyPlace, argv) {
 
     var vm = this;
-    vm.color = 'D';
     vm.labelSelectPlaceHolder = 'Select from below or type label name…';
-    vm.targetLimit = 22;
-    vm.haveMore = false;
-    vm.searchFn = search;
-    vm.search = {};
+    vm.targetLimit = 100;
+    vm.haveMore = true;
+    vm.searchFn = _.debounce(search, 100);
+    vm.oldKeyword = '';
+    vm.loading = false;
+    vm.updatedRecipient = updatedRecipient;
+    vm.search = {
+      results: []
+    };
+    vm.goDownward = false;
+    vm.load = load;
     vm.selectedLabels = argv.addedLabels.length > 0 ? argv.addedLabels : [];
-    vm.firstItemsLength = vm.selectedLabels.length;
+    vm.firstTouch = false;
+    var defaultLimit = 8;
     vm.setting = {
       skip: 0,
-      limit: 8 + vm.selectedLabels.length,
-      filter: 'all',
+      limit: defaultLimit + vm.selectedLabels.length,
+      filter: NST_LABEL_SEARCH_FILTER.MY_LABELS,
     }
 
-    search('');
+    vm.load();
 
     vm.submitLabels = function (){
       $uibModalInstance.close(vm.selectedLabels);
     }
 
     function search(keyword){
+      if (typeof keyword !== 'string' || vm.loading) return;
+      vm.loading = true;
+      if ( keyword !== vm.oldKeyword) {
+        restoreDefault();
+      }
       NstSvcLabelFactory.search(keyword, vm.setting.filter, vm.setting.skip, vm.setting.limit).then(function (items){
-        // TODO remove  vm.selectedLabels items from results
-        console.log(items, vm.selectedLabels);
+
         var sameItems = _.intersectionBy(items, vm.selectedLabels, 'title');
-        vm.search.results = _.difference(items, sameItems);;
+        var newItems = _.difference(items, sameItems).splice(0, defaultLimit);
+
+        vm.search.results.push.apply(vm.search.results, newItems);
+
+        // console.log(items.length, sameItems.length, items.length - (vm.selectedLabels.length - sameItems.length),vm.setting.skip);
+        vm.setting.skip += items.length - (vm.selectedLabels.length - sameItems.length);
+        vm.oldKeyword = keyword;
+        vm.haveMore = vm.setting.limit === items.length;
+        vm.loading = false;
+
+        $timeout(function() {
+          vm.goDownward = true;
+        },100)
       });
     }
-    
+    function load() {
+      vm.searchFn('');
+    }
+    function restoreDefault() {
+      vm.setting.skip = 0;
+      vm.search.results = [];
+      vm.haveMore = true;
+    }
+    function updatedRecipient(onSelect, label) {
+      vm.firstTouch = true;
+      $scope.$broadcast('SetFocus');
+    }
   }
 
 })();
