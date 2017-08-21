e48f7f5f76e7405055824dcafe2023e6f3a140d5
hamidrezakk <hamidrezakks@gmail.com>
2017-08-21 11:56:24 +0430

    Miniplayer service updated

--- src/nested/mini-player/mini-player.service.js
+++ src/nested/mini-player/mini-player.service.js
@@ -6,17 +6,21 @@
 
   /** @ngInject */
   function SvcMiniPlayer($rootScope,$window,deviceDetector,$timeout, NstSvcFileFactory, NstSvcStore, _) {
-    var obj = {},
-      audioDOMS= [],
-      audioObjs = [],
-      currentlyPlay;
+    var audioDOM;
+    var audioObjs = [];
+    var currentlyPlay = null;
 
     function MiniPlayer () {
+      audioDOM = document.createElement('audio');
+      audioDOM.style.display = 'none';
+      document.body.appendChild(audioDOM);
 
+      audioDOM.onended = function () {
+        $rootScope.$broadcast('play-audio', '');
+      };
     }
 
     MiniPlayer.prototype.constructor = MiniPlayer;
-
     MiniPlayer.prototype.addTrack = addTrack;
     MiniPlayer.prototype.play = play;
     MiniPlayer.prototype.pause = pause;
@@ -24,74 +28,75 @@
     MiniPlayer.prototype.getCurrent = getCurrent;
     MiniPlayer.prototype.getList = getList;
 
-    function addTrack (item) {
-      var alreadyCreated = audioDOMS.find(function (audioDOM) {
-        return audioDOM.className === item.id;
-      });
+    var service = new MiniPlayer();
+    return service;
 
-      if (alreadyCreated) {
-        // alreadyCreated.pause();
-        // alreadyAdded.remove();
-        // pauseCallback(false);
-        // _.remove(audioDOMS, function(n) {
-        //     return n.className === item.id;
-        // });
-        return ;
-      }
+    function addTrack (item) {
+      // var alreadyCreated = audioDOMS.find(function (audioDOM) {
+      //   return audioDOM.className === item.id;
+      // });
+      //
+      // if (alreadyCreated) {
+      //   // alreadyCreated.pause();
+      //   // alreadyAdded.remove();
+      //   // pauseCallback(false);
+      //   // _.remove(audioDOMS, function(n) {
+      //   //     return n.className === item.id;
+      //   // });
+      //   return ;
+      // }
       audioObjs.push(item);
 
-      var audio = document.createElement('audio');
-      audio.style.display = 'none';
-      audio.className = item.id;
-      audio.autoplay = item.isPlayed ? true : false;
-      audio.src = item.src;
-      document.body.appendChild(audio);
+      // var audio = document.createElement('audio');
+      // audio.style.display = 'none';
+      // audio.className = item.id;
+      // audio.autoplay = item.isPlayed ? true : false;
+      // audio.src = item.src;
+      // document.body.appendChild(audio);
 
-      audioDOMS.push(audio);
+      audioDOM.className = item.id;
+      audioDOM.autoplay = item.isPlayed ? true : false;
+      audioDOM.src = item.src;
+      audioDOM.load();
 
-      audio.onended = function(){
-        // audio.remove() //Remove when played.
-        $rootScope.$broadcast('play-audio', '');
-        // _.remove(audioDOMS, function(n) {
-        //     return n.className === item.id;
-        // });
-      };
+      // audioDOMS.push(audio);
+
+      this.play(item.id);
     }
 
     function play (id) {
+      if (currentlyPlay !== null) {
+        var playingItem = this.getCurrent();
+        this.pause(playingItem.item.id);
+      }
       currentlyPlay = id;
-      var DOM = audioDOMS.find(function (audioDOM) {
-        return audioDOM.className === id;
-      });
-      DOM.play();
+      // var DOM = audioDOMS.find(function (audioDOM) {
+      //   return audioDOM.className === id;
+      // });
+      audioDOM.play();
+      console.log(id, 'is playing');
       $rootScope.$broadcast('play-audio', id);
     }
 
     function pause (id) {
       currentlyPlay = '';
-      var DOM = audioDOMS.find(function (audioDOM) {
-        return audioDOM.className === id;
-      });
-      DOM.pause();
+      audioDOM.pause();
       $rootScope.$broadcast('play-audio', '');
     }
 
     function removeAll () {
-      audioDOMS.forEach(function (i){
-        i.pause();
-        i.remove();
-      });
-      audioDOMS = [];
+      audioDOM.pause();
+
       audioObjs = [];
       $rootScope.$broadcast('play-audio', '');
     }
 
     function getCurrent () {
-      var index = _.findIndex(obj.audioObjs, function (o) {
-        return o.id === obj.currentlyPlay
+      var index = _.findIndex(audioObjs, function (o) {
+        return o.id === currentlyPlay
       });
       return {
-        item : obj.audioObjs[index],
+        item : audioObjs[index],
         index : index
       }
     }
@@ -99,7 +104,5 @@
     function getList () {
       return audioObjs;
     }
-
-    return MiniPlayer;
   }
 })();
