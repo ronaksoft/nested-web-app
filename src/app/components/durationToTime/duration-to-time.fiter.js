89b4f400c84f8e46769dff13de1d85628dc9f5c2
hamidrezakk <hamidrezakks@gmail.com>
2017-08-22 12:02:46 +0430

    Updated

---
+++ src/app/components/durationToTime/duration-to-time.fiter.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ronak.nested.web.components.text')
+    .filter('durationToTime', function(){
+      return function(duration) {
+        duration = Math.floor(parseInt(duration));
+        var isNegative = false;
+        if (duration < 0) {
+          duration = -duration;
+          isNegative = true;
+        }
+        var second = duration%60;
+        second = second.toString();
+        if (second.length === 1) {
+          second = '0' + second;
+        }
+        var minute = Math.floor(duration/60);
+        minute = minute.toString();
+        if (minute.length === 1) {
+          minute = '0' + minute;
+        }
+        return (isNegative? '-' : '') + minute + ':' + second
+      };
+    });
+})();
