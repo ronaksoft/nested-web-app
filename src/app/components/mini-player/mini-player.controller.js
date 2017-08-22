9b40f4d29cc75a6e2fceac801623e9075d73d756
robzizo <me@robzizo.ir>
2017-08-22 12:00:29 +0430

    current time of player

--- src/app/components/mini-player/mini-player.controller.js
+++ src/app/components/mini-player/mini-player.controller.js
@@ -32,8 +32,9 @@
 
 
       SvcMiniPlayer.timeChanged(function (t) {
+        console.log(t);
         $scope.$apply(function () {
-          vm.currentTime = Math.floor(t);
+          vm.currentTime = t;
         });
       });
 
