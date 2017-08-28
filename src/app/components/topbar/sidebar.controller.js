dbff1ab835db7b970c8c264420a2fc0b2801c56f
robzizo <me@robzizo.ir>
2017-08-28 09:41:56 +0430

    new ui initial commit

---
+++ src/app/components/topbar/sidebar.controller.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular
+      .module('ronak.nested.web.components.sidebar')
+      .controller('TopBarController', TopBarController);
+
+    /** @ngInject */
+    function TopBarController($q, $scope, $state, $stateParams, $uibModal, $rootScope,
+                               _) {
+      var vm = this;
+      
+    }
+  })();
