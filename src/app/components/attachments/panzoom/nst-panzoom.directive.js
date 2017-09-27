a913fa8e2a8a0d8d93356e61a743c21bde40e396
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-09-27 13:49:26 +0330

    now we have zoom and pan ability, plus minor bug fixed

---
+++ src/app/components/attachments/panzoom/nst-panzoom.directive.js
@@ -0,0 +1,96 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ronak.nested.web.components.attachment')
+    .directive('nstPanzoom', NstPanzoom);
+
+  function NstPanzoom(NST_FILE_TYPE, $sce, $window, NstSvcKeyFactory, NST_KEY, _, $) {
+    return {
+      restrict: 'A',
+      scope: {
+        containerClass: '=nstPanzoom',
+      },
+      link: function ($scope, $element, attrs) {
+        var eventReferences = [];
+        var template =
+          '<div class="nst-panzoom-container">' +
+            '<div class="nst-panzoom-item" rel="reset"><span class="unfilled-rect"></span></div>' +
+            '<div class="nst-panzoom-item" rel="in">+</div>' +
+            '<div class="nst-panzoom-item" rel="out">-</div>' +
+          '</div>';
+        $element.parents($scope.containerClass).append(template);
+
+        $element.addClass('cursor-pan');
+
+        var zoom = 1.0;
+        var pan = {
+          x: 0,
+          y: 0
+        };
+        var origin = {
+          x: 0,
+          y: 0
+        };
+
+        var panStartPos = {
+          x: 0,
+          y: 0
+        };
+
+        var startPan = false;
+        var toolsElem = angular.element($scope.containerClass + ' .nst-panzoom-container');
+        eventReferences.push(toolsElem.find('.nst-panzoom-item[rel="in"]').on('click', function () {
+          zoom += 0.1;
+          applyChanges();
+        }));
+        eventReferences.push(toolsElem.find('.nst-panzoom-item[rel="out"]').on('click', function () {
+          zoom -= 0.1;
+          applyChanges();
+        }));
+        eventReferences.push(toolsElem.find('.nst-panzoom-item[rel="reset"]').on('click', function () {
+          zoom = 1.0;
+          pan.x = 0;
+          pan.y = 0;
+          applyChanges();
+        }));
+        eventReferences.push($element.on('mousedown', function (e) {
+          startPan = true;
+          panStartPos.x = e.pageX - pan.x;
+          panStartPos.y = e.pageY - pan.y;
+          applyChanges();
+        }));
+        eventReferences.push(angular.element('body').on('mousemove', function (e) {
+          if (startPan) {
+            e.preventDefault();
+            pan.x = (e.pageX - panStartPos.x);
+            pan.y = (e.pageY - panStartPos.y);
+            applyChanges();
+          }
+        }));
+        eventReferences.push(angular.element('body').on('mouseup', function (e) {
+          if (startPan) {
+            e.preventDefault();
+          }
+          startPan = false;
+        }));
+
+        function applyChanges() {
+          if (zoom < 0.1) {
+            zoom = 0.1;
+          }
+          if (zoom > 10) {
+            zoom = 10;
+          }
+          $element.css('transform', 'scale(' + zoom + ') translate(' + (pan.x/zoom) + 'px, ' + (pan.y/zoom) + 'px)');
+        }
+
+        $scope.$on('$destroy', function() {
+          _.forEach(eventReferences, function (event) {
+            event.off();
+          });
+        });
+      }
+    }
+  }
+})();
