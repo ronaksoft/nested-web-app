8367f49a02c137e1c4c98e04ed5c32c74999e3bc
robzizo <me@robzizo.ir>
2017-09-27 14:02:14 +0330

    progressbar files

---
+++ src/app/task/common/task-progress/task-sidebar.controller.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    angular
+      .module('ronak.nested.web.task')
+      .controller('taskProgressIconController', taskProgressIconController);
+
+    /** @ngInject */
+    function taskProgressIconController($q, $scope, $state, $stateParams, $uibModal, $rootScope) {
+      var vm = this;
+      // var eventReferences = [];
+      var sit = 
+      ['not-assigned', 'assigned', 'overdue', 'rejected', 'hold', 'complete']
+      vm.hasChecklist = false;
+
+      vm.status = 'assigned';
+      
+
+
+    }
+  })();
