8e859698c08bb21175b221cd4a21d8a4d546acfd
robzizo <me@robzizo.ir>
2017-10-07 15:31:21 +0330

    create task  initial commit

--- src/app/task/common/sidebar/task-sidebar.controller.js
+++ src/app/task/common/sidebar/task-sidebar.controller.js
@@ -7,8 +7,21 @@
 
     /** @ngInject */
     function TaskSidebarController($q, $scope, $state, $stateParams, $uibModal, $rootScope) {
-      // var vm = this;
+      var vm = this;
       // var eventReferences = [];
+
+      vm.createTask = createTask
+
+      function createTask(){
+        $uibModal.open({
+          animation: false,
+          size: 'create-task',
+          templateUrl: 'app/task/pages/create-task/create-task.html',
+          controller: 'createTaskController',
+          controllerAs: 'ctrl',
+          backdropClass: 'taskBackDrop'
+        })
+      }
 
     }
   })();
---
+++ src/app/task/pages/create-task/create-task.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+      .module('ronak.nested.web.task')
+      .controller('createTaskController', createTaskController);
+
+    /** @ngInject */
+    function createTaskController($q, $scope, $state, $stateParams, $uibModal, $rootScope) {
+      var vm = this;
+      // var eventReferences = [];
+
+
+    }
+  })();
