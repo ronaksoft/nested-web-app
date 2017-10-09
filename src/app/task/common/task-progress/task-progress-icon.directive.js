0af41339887ae075a7c1527e6b7b600f7a2f060e
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-10-09 13:40:33 +0330

    task progress icon added

--- src/app/task/common/task-progress/task-progress-icon.directive.js
+++ src/app/task/common/task-progress/task-progress-icon.directive.js
@@ -6,14 +6,22 @@
     .directive('taskProgressIcon', taskProgress);
 
   /** @ngInject */
-  function taskProgress() {
+  function taskProgress(NST_TASK_PROGRESS_ICON) {
     return {
       restrict: 'E',
       templateUrl: 'app/task/common/task-progress/task-progress-icon.html',
-      controller: 'taskProgressIconController',
-      controllerAs: 'ctrl',
-      link : function ($scope, $el, $attrs){
-
+      scope: {
+        status: '=status',
+        progress: '=progress'
+      },
+      link: function ($scope) {
+        $scope.statuses = NST_TASK_PROGRESS_ICON;
+        if ($scope.status === NST_TASK_PROGRESS_ICON.ASSIGNED_PROGRESS && $scope.progress !== undefined) {
+          var ratio = ($scope.progress/100)*57;
+          setTimeout(function () {
+            angular.element('.task-progress-icon svg.progress circle').css('stroke-dasharray', ratio + ' 60');
+          }, 1000);
+        }
       }
     };
   }
