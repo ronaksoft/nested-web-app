11f5eb9848723306285f154378a8cd7373efd279
robzizo <me@robzizo.ir>
2017-10-10 11:25:33 +0330

    place chips

--- src/app/components/chips/place-chips.directive.js
+++ src/app/components/chips/place-chips.directive.js
@@ -1,1 +1,68 @@
- 
+(function () {
+    
+      'use strict';
+    
+      angular
+        .module('ronak.nested.web.components')
+        .directive('placeChips', placeChips);
+    
+    
+      function placeChips($templateCache, $compile, $document, datesCalculator, ngJalaaliFDP, moment, NstSvcI18n,
+        NstSvcPlaceFactory) {
+    
+        return {
+          restrict: 'A',
+          scope: {
+            placeId: '=',
+            isEmail: '=',
+            onClear: '='
+          },
+          link: function (scope, element, attrs) {
+            scope.isSelected = false;
+            
+            var template = angular.element($templateCache.get('place-chips.html'));
+            console.log(scope.placeId);
+            NstSvcPlaceFactory.get(scope.placeId, false).then(function(place){
+              console.log(place);
+              scope.place = place;
+              init();
+            }).catch(function(e){
+              console.log(e)
+            })
+            /**
+             * Init the directive
+             * @return {}
+             */
+            function init() {
+    
+              $compile(template)(scope);
+              element.html(template);
+            }
+    
+            scope.selectChip = function (){
+              scope.isSelected = true;
+            }
+            scope.unselectChip = function (){
+              scope.isSelected = false;
+            }
+            var onDocumentClick = function (event) {
+              if (element[0] !== event.target) {
+                scope.isSelected = false;
+              }
+            };
+            $document.on('click', onDocumentClick);
+            scope.closeChip = function (){
+              if (typeof scope.onClear === 'function') {
+                scope.onClear();
+              }
+            }
+            element.on('$destroy', function () {
+              $document.off('click', onDocumentClick);
+            });
+    
+          }
+        };
+      }
+    
+    })();
+    
