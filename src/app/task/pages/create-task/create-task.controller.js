a153ddd1689126520b2a62e98da7a0a69d1d4e0f
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-10-10 11:46:46 +0330

    assignee chips added

--- src/app/task/pages/create-task/create-task.controller.js
+++ src/app/task/pages/create-task/create-task.controller.js
@@ -6,13 +6,36 @@
       .controller('createTaskController', createTaskController);
 
     /** @ngInject */
-    function createTaskController($q, $scope, $state, $stateParams, $uibModal, $rootScope, NstSvcAuth) {
+    function createTaskController($q, $scope, $state, $stateParams, $uibModal, $rootScope, NstSvcAuth, _) {
       var vm = this;
       // var eventReferences = [];
       vm.showMoreOption = false
       vm.user = NstSvcAuth.user
-      console.log(vm.user)
 
+      vm.assigneeInput = '';
+      vm.assigneeKeyDown = assigneeKeyDown;
+      vm.assignees = [];
+
+      function assigneeKeyDown(event) {
+        if (event.keyCode === 13) {
+          _.forEach(parseMentionData(vm.assigneeInput), function (item) {
+            vm.assignees.push(item);
+          });
+          vm.assignees = _.uniq(vm.assignees);
+          vm.assigneeInput = '';
+        }
+      }
+
+      function parseMentionData(data) {
+        data = data.split(',');
+        data = _.map(data, function (item) {
+          return _.trim(item);
+        });
+        data = _.filter(data, function (item) {
+          return item.length > 1;
+        });
+        return data;
+      }
 
     }
   })();
