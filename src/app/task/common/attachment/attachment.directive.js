a1a8eae56c732be5d088e57575293fbfa0857111
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-10-16 12:39:27 +0330

    attachment updated

---
+++ src/app/task/common/attachment/attachment.directive.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ronak.nested.web.task')
+    .directive('taskAttachment', taskAttachment);
+
+  /** @ngInject */
+  function taskAttachment() {
+    return {
+      restrict: 'E',
+      templateUrl: 'app/task/common/attachment/attachment.html',
+      controller: 'TaskAttachmentController',
+      controllerAs: 'ctlAttachment',
+      scope: {},
+      bindToController: {
+
+      }
+    };
+  }
+
+})();
