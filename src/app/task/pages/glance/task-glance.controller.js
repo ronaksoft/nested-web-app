1874addc097a24ca483aef55e475bb86fe2dde9c
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-10-21 16:15:07 +0330

    fix

--- src/app/task/pages/glance/task-glance.controller.js
+++ src/app/task/pages/glance/task-glance.controller.js
@@ -5,13 +5,30 @@
     .module('ronak.nested.web.task')
     .controller('taskGlanceController', taskGlanceController);
 
-  function taskGlanceController($rootScope, $scope, $state) {
+  function taskGlanceController($rootScope, $scope, _, $state, NstSvcTaskFactory, NST_TASK_STATUS) {
     var vm = this;
     // var eventReferences = [];
 
     // vm.user = NstSvcAuth.user;
+    vm.taskSetting = {
+      limit: 8,
+      skip: 0
+    };
+
+    vm.tasks = [];
+
+    loadTasks();
 
     vm.editTask = editTask;
+
+    function loadTasks() {
+      NstSvcTaskFactory.getByFilter(NST_TASK_STATUS.CREATED_BY_ME, null, vm.taskSetting.skip, vm.taskSetting.limit).then(function (tasks) {
+        vm.tasks = vm.tasks.concat(tasks);
+        _.uniqBy(vm.tasks, 'id');
+        vm.taskSetting.skip = vm.tasks.length;
+        console.log(vm.tasks);
+      });
+    }
 
     function editTask(id) {
       $state.go('app.task.edit', {
