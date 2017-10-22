9179b397a415caabc702e505b1ae17921e911acd
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-10-22 15:11:31 +0330

    task updated

--- src/app/task/pages/glance/task-glance.controller.js
+++ src/app/task/pages/glance/task-glance.controller.js
@@ -5,29 +5,139 @@
     .module('ronak.nested.web.task')
     .controller('taskGlanceController', taskGlanceController);
 
-  function taskGlanceController($rootScope, $scope, _, $state, NstSvcTaskFactory, NST_TASK_STATUS, NstSvcTaskUtility) {
+  function taskGlanceController($rootScope, $scope, _, $state, NstSvcTaskFactory, NST_TASK_STATUS, NstSvcTaskUtility, $timeout) {
     var vm = this;
     var eventReferences = [];
 
     // vm.user = NstSvcAuth.user;
+    vm.loading = true;
+    vm.firstTimeLoading = true;
     vm.taskSetting = {
       limit: 8,
       skip: 0
     };
 
-    vm.tasks = [];
-
-    loadTasks();
-
+    vm.isGlancePage = false;
+    vm.isAssignedToMePage = false;
+    vm.isCreatedByMePage = false;
+    vm.isWatchlistPage = false;
+    vm.isCustomFilterPage = false;
     vm.editTask = editTask;
     vm.getTaskIcon = NstSvcTaskUtility.getTaskIcon;
 
+    vm.overDueTasks = [];
+    vm.pendingTasks = [];
+    vm.tasks = [];
+
+    setLocationFlag();
+    loadTasks();
+    getOverdueTasks();
+    getPendingTasks();
+
+    function setLocationFlag() {
+      switch ($state.current.name) {
+        default:
+        case 'app.task.glance':
+          vm.isGlancePage = true;
+          break;
+        case 'app.task.assigned_to_me':
+          vm.isAssignedToMePage = true;
+          break;
+        case 'app.task.created_by_me':
+          vm.isCreatedByMePage = true;
+          break;
+        case 'app.task.watchlist':
+          vm.isCreatedByMePage = true;
+          break;
+        case 'app.task.custom_filter':
+          vm.isCustomFilterPage = true;
+          break;
+      }
+    }
+
+    function mergeTask(tasks) {
+      var newItems = _.differenceBy(tasks, vm.tasks, 'id');
+      // var removedItems = _.differenceBy(vm.tasks, tasks, 'id');
+      //
+      // _.forEach(removedItems, function (item) {
+      //   var index = _.findIndex(vm.tasks, {
+      //     'id': item.id
+      //   });
+      //   if (index > -1) {
+      //     vm.tasks.splice(index, 1);
+      //   }
+      // });
+
+      vm.tasks.unshift.apply(vm.tasks, newItems);
+    }
+
+    function getOverdueTasks() {
+      NstSvcTaskFactory.getByFilter(NST_TASK_STATUS.ASSIGNED_TO_ME, NST_TASK_STATUS.OVERDUE).then(function (tasks) {
+        vm.overDueTasks = tasks;
+      });
+    }
+
+    function getPendingTasks() {
+      NstSvcTaskFactory.getByFilter(NST_TASK_STATUS.CANDIDATE, NST_TASK_STATUS.NO_ASSIGNED).then(function (tasks) {
+        vm.pendingTasks = tasks;
+      });
+    }
+
+    function getTasks() {
+      vm.loading = true;
+      var promise;
+      promise = NstSvcTaskFactory.getByFilter(NST_TASK_STATUS.CREATED_BY_ME, null, vm.taskSetting.skip, vm.taskSetting.limit);
+
+      promise.then(function (tasks) {
+        vm.reachedTheEnd = tasks.length < vm.taskSetting.limit;
+      }).catch(function () {
+
+      }).finally(function () {
+        vm.loading = false;
+      });
+
+      return promise;
+    }
+
+    function appendTasks(tasks) {
+      var items = _.differenceBy(tasks, vm.tasks, 'id');
+      vm.tasks.push.apply(vm.tasks, items);
+    }
+
     function loadTasks() {
-      NstSvcTaskFactory.getByFilter(NST_TASK_STATUS.CREATED_BY_ME, null, vm.taskSetting.skip, vm.taskSetting.limit).then(function (tasks) {
-        vm.tasks = vm.tasks.concat(tasks);
-        _.uniqBy(vm.tasks, 'id');
+      getTasks().then(function (tasks) {
+        mergeTask(tasks);
+        vm.taskSetting.skip = vm.tasks.length;
+
+        // to full fill page at first loading
+        if (vm.firstTimeLoading) {
+          vm.firstTimeLoading = false;
+          $timeout(function () {
+            if (!isPageFilled()) {
+              loadMoreTasks();
+            }
+          }, 100);
+        }
+      });
+    }
+
+    function loadMoreTasks() {
+      if (vm.loading && !vm.reachedTheEnd) {
+        return;
+      }
+      getTasks().then(function (tasks) {
+        appendTasks(tasks);
         vm.taskSetting.skip = vm.tasks.length;
       });
+    }
+
+    function isPageFilled() {
+      var taskContainer = angular.element('#task-container');
+      if (window.innerHeight > taskContainer.offset().top + taskContainer.height()) {
+        return false;
+      } else {
+        return true;
+      }
     }
 
     function editTask(id) {
@@ -36,11 +146,17 @@
       });
     }
 
+    /*
+     * Events
+     */
     eventReferences.push($rootScope.$on('task-created', function () {
       vm.taskSetting.limit = 8;
       vm.taskSetting.skip = 0;
-      vm.tasks = [];
       loadTasks();
+    }));
+
+    eventReferences.push($rootScope.$on('scroll-reached-bottom', function () {
+      loadMoreTasks();
     }));
 
     $scope.$on('$destroy', function () {
