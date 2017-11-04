f197ebe8daf29f7f142d469d8ac5760713d643c4
robzizo <me@robzizo.ir>
2017-11-04 11:15:44 +0330

    view service malo'oon line

--- src/nested/interface/view.service.js
+++ src/nested/interface/view.service.js
@@ -188,7 +188,8 @@
       if (thisEl) {
         thisDeterminer = thisEl.postCardOffTop + thisEl.postCardheight - firstOffset;
         if (thisDeterminer < Ypos + winH && thisDeterminer > Ypos ) {
-          return $rootScope.inViewPost.enabled = true; // FIXME it stops applying to first item in view
+          // return $rootScope.inViewPost.enabled = true; // FIXME it stops applying to first item in view
+          $rootScope.inViewPost.enabled = true;
         } else {
           $rootScope.inViewPost.enabled = false;
         }
