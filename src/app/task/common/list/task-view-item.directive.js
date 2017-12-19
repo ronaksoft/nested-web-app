925b0675c9d0e4237467800c9a5be3a1acf38fd9
robzizo <me@robzizo.ir>
2017-12-19 16:27:12 +0330

    task item view issue

--- src/app/task/common/list/task-view-item.directive.js
+++ src/app/task/common/list/task-view-item.directive.js
@@ -12,17 +12,43 @@
         replace: true,
         templateUrl: 'app/task/common/list/task-view-item.html',
         link: function ($scope, $el) {
-            $scope.ctrl = {
-                statuses: NST_TASK_STATUS,
-                editTask: function (id) {
-                    $state.go('app.task.edit', {
-                      taskId: id
-                    }, {
-                      notify: false
-                    });
-                },
-                getTaskIcon: NstSvcTaskUtility.getTaskIcon
+          function setLocationFlag() {
+            switch ($state.current.name) {
+              default:
+              case 'app.task.glance':
+                $scope.ctrl.isGlancePage = true;
+                break;
+              case 'app.task.assigned_to_me':
+                $scope.ctrl.isAssignedToMePage = true;
+                break;
+              case 'app.task.created_by_me':
+                $scope.ctrl.isCreatedByMePage = true;
+                break;
+              case 'app.task.watchlist':
+                $scope.ctrl.isWatchlistPage = true;
+                break;
+              case 'app.task.custom_filter':
+                $scope.ctrl.isCustomFilterPage = true;
+                break;
             }
+          }
+          $scope.ctrl = {
+            statuses: NST_TASK_STATUS,
+            isGlancePage: false,
+            isAssignedToMePage: false,
+            isCreatedByMePage: false,
+            isWatchlistPage: false,
+            isCustomFilterPage: false,
+            editTask: function (id) {
+                $state.go('app.task.edit', {
+                  taskId: id
+                }, {
+                  notify: false
+                });
+            },
+            getTaskIcon: NstSvcTaskUtility.getTaskIcon
+          }
+          setLocationFlag();
         }
       };
     }
