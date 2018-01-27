0c7702d8f2d77a17971961f008b568f53bdf6944
robzizo <me@robzizo.ir>
2018-01-27 16:08:27 +0330

    task item in post view

---
+++ src/app/messages/partials/task-card/task-card.controller.js
@@ -0,0 +1,25 @@
+/**
+ * @file app/messages/post-card.controller.js
+ * @author robzizo < me@robzizo.ir >
+ * @description controller for post card directive
+ *              Documented by:          robzizo
+ *              Date of documentation:  2017-07-25
+ *              Reviewed by:            -
+ *              Date of review:         -
+ */
+(function () {
+    'use strict';
+  
+    angular
+      .module('ronak.nested.web.message')
+      .controller('TaskCardController', TaskCardController);
+  
+    function TaskCardController($state, $log, $timeout, $stateParams, $rootScope, $scope, $uibModal,
+                                _, NST_TASK_PROGRESS_ICON, NstSvcAuth, $) {
+      var vm = this;
+      vm.statuses = NST_TASK_PROGRESS_ICON;
+
+    }
+  
+  })();
+  
