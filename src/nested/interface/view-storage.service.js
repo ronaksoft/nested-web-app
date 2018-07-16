fd0c682adc14cf476a5cb953fd2fd7d7624d946d
Hamidreza KK <hamidrezakks@gmail.com>
2018-07-16 15:09:43 +0430

    updated

--- src/nested/interface/view-storage.service.js
+++ src/nested/interface/view-storage.service.js
@@ -5,10 +5,13 @@
     .service('NstSvcViewStorage', NstSvcViewStorage);
 
   /** @ngInject */
-  function NstSvcViewStorage(NST_STORAGE_TYPE, NstStorage) {
+  function NstSvcViewStorage(NST_STORAGE_TYPE, NstStorage, NstSvcServer) {
 
     function ViewStorage() {
       this.places = {};
+      this.myPlacesId = [];
+      this.page = 0;
+      getMyPlaces();
     }
 
     ViewStorage.prototype = new NstStorage(NST_STORAGE_TYPE.LOCAL, 'view');
@@ -24,6 +27,27 @@
 
     function getByPlace(placeId) {
       return service.places[placeId];
+    }
+
+    function getAllPlaces(skip, limit) {
+      return NstSvcServer.request('account/get_all_places', {
+        with_children: true,
+        skip: skip,
+        limit: limit
+      });
+    }
+
+    function getMyPlaces() {
+      var limit = 100;
+      getAllPlaces(service.page * limit, service.limit).then(function (res) {
+        res.places.forEach(function (place) {
+          service.myPlacesId.push(place._id);
+        });
+        if (res.places.length === limit) {
+          service.page++;
+          getMyPlaces();
+        }
+      });
     }
 
     var service = new ViewStorage();
