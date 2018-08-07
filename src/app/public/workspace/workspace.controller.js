7d2da4e46979da963ef68b37dbd0864809944b49
Hamidreza KK <hamidrezakks@gmail.com>
2018-08-07 13:15:32 +0430

    workspace policy changed

--- src/app/public/workspace/workspace.controller.js
+++ src/app/public/workspace/workspace.controller.js
@@ -7,6 +7,8 @@
 
   function workspaceController($scope, $state, _, toastr, NstSvcTranslation, NstSvcServer) {
 
+    var NST_SERVER_DOMAIN = 'nested.server.domain';
+
     var eventReferences = [];
     var vm = this;
     vm.progress = false;
@@ -14,9 +16,27 @@
 
     vm.submitForm = submitForm;
 
+    (function () {
+      if (!$state.params.force || $state.params.force !== 'true') {
+        var domain = localStorage.getItem(NST_SERVER_DOMAIN);
+        if (domain && domain.length > 3) {
+          $state.go('public.domain-redirect', {domain: domain});
+        } else {
+          var host = window.location.host;
+          if (['nested.me', 'web.nested.me', 'webapp.nested.me'].indexOf(host) === -1) {
+            changeWorkspace(host);
+          }
+        }
+      }
+    })();
+
     function submitForm() {
-      NstSvcServer.setDomain(vm.workspace).then(function () {
-        $state.go('public.domain-redirect', {domain: vm.workspace});
+      changeWorkspace(vm.workspace);
+    }
+
+    function changeWorkspace(domain) {
+      NstSvcServer.setDomain(domain).then(function () {
+        $state.go('public.domain-redirect', {domain: domain});
       }).catch(function () {
         toastr.error(NstSvcTranslation.get('Invalid domain'));
       });
