9dd9469380756f29ec681df3c53de3848cda7eec
Hamidreza KK <hamidrezakks@gmail.com>
2018-08-15 12:19:08 +0430

    updated

--- src/app/public/workspace/config-finder/config-finder.service.js
+++ src/app/public/workspace/config-finder/config-finder.service.js
@@ -16,34 +16,33 @@
 
     function getConfig(host) {
       var deferred = $q.defer();
-      // if (['nested.me', 'web.nested.me', 'webapp.nested.me'].indexOf(host) === -1) {
-        NstSvcServer.setDomain(host).then(function () {
-          deferred.resolve(host);
-        }).catch(function () {
-          if (typeof host !== 'string') {
-            deferred.reject();
-            return
-          }
-          var parts = host.split('.');
-          if (parts.length > 2) {
-            parts = parts.reverse();
-            var d = parts[1] + '.' + parts[0];
-            if (d !== 'nested.me') {
-              NstSvcServer.setDomain(d).then(function () {
-                deferred.resolve(host);
-              }).catch(function (reason) {
-                deferred.reject(reason);
-              });
-            } else {
-              deferred.reject();
-            }
+      if (['web.nested.me', 'webapp.nested.me'].indexOf(host) > -1) {
+        host = 'nested.me';
+      }
+      NstSvcServer.setDomain(host).then(function () {
+        deferred.resolve(host);
+      }).catch(function () {
+        if (typeof host !== 'string') {
+          deferred.reject();
+          return
+        }
+        var parts = host.split('.');
+        if (parts.length > 2) {
+          parts = parts.reverse();
+          var d = parts[1] + '.' + parts[0];
+          if (d !== 'nested.me') {
+            NstSvcServer.setDomain(d).then(function () {
+              deferred.resolve(host);
+            }).catch(function (reason) {
+              deferred.reject(reason);
+            });
           } else {
             deferred.reject();
           }
-        });
-      // } else {
-      //   deferred.reject();
-      // }
+        } else {
+          deferred.reject();
+        }
+      });
       return deferred.promise;
     }
 
