e6c10e56c3e406c71ab3a0432c57f236bbfaba7b
robzizo <me@robzizo.ir>
2019-07-02 12:36:10 +0430

    river instance bugfix

--- src/app/authenticate/logout/logout.controller.js
+++ src/app/authenticate/logout/logout.controller.js
@@ -22,6 +22,6 @@
     }
     document.getElementById('river-embed').remove();
     $state.go('public.signin');
-    window.riverServiceInstance.destroy();
+    window.riverServiceInstance && window.riverServiceInstance.destroy();
   }
 })();
